9a191a2234adb4a05d02683f324ab328836f56a1
Joel Gales <jomoga@users.sourceforge.net>
2006-03-01 19:45:41 +0000

    Add GPL header

--- src/mpi.cpp
+++ src/mpi.cpp
@@ -1,8 +1,11 @@
-//
-//  GDL implmenetation of MPI wrappers
-//  (C) Tech-X Corporation
-//
-//
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 //#include <config.h>
 #include "includefirst.hpp"
