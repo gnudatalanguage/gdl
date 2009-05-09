47419cf43a2cac5f20c40f464892e274effa87a9
Joel Gales <jomoga@users.sourceforge.net>
2009-05-09 17:30:13 +0000

    Zero out arrays of necessary

--- src/export.hpp
+++ src/export.hpp
@@ -9,6 +9,7 @@ template< typename T1, typename T2>
 T2* GDL_GetScratch(GDL_VPTR *p, DLong nElem)
 {
   BaseGDL* data = new T1( dimension( nElem), GDL_BARR_INI_ZERO);
+  bzero( data->DataAddr(), nElem*sizeof(T2));
   *p = data;
   return (T2 *) data->DataAddr();
 }
@@ -18,6 +19,13 @@ T2* GDL_MakeTempArray( DLong rank, DLong *dim, BaseGDL::InitType iT,
 		       GDL_VPTR *v)
 {
   BaseGDL* data = new T1( dimension(dim, rank), iT);
+
+  if ( iT == GDL_BARR_INI_ZERO) {
+    DLong nElem = 1;
+    for( SizeT i=0; i<rank; i++) nElem *= dim[i];
+    bzero( data->DataAddr(), nElem*sizeof(T2));
+  }
+
   *v = data;
   return (T2 *) data->DataAddr();
 }
