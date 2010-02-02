e76715453d3631151cf25d7405bc68853aa29b1d
Mark Schellens <mschellens@gmail.com>
2010-02-02 21:26:59 +0000

    add file

---
+++ src/lapack.cpp
@@ -0,0 +1,19 @@
+/***************************************************************************
+                          lapack.cpp  -  lapack routines
+                             -------------------
+    begin                : January 31 2010
+    copyright            : (C) 2010y Marc Schellens
+    email                : m_schellens@users.sf.net
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+//#include <lapackpp.h>
+
