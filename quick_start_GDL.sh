49843baaa096cea286477b2bdf4ad84907e1f65e
Alain Coulais <alain.coulais@obspm.fr>
2010-10-01 22:25:16 +0000

    adding the option for keeping current GDL_PATH when running "fast launcher"

--- quick_start_GDL.sh
+++ quick_start_GDL.sh
@@ -5,7 +5,8 @@
 # AC: improved shell to avoid "big" when sourcing (or ".") this file ...
 # echo "(ba)sh or source ? "$BASH_SOURCE
 #
-
+# AC 01/10/2010: we can keep the current $GDL_PATH using -g [--gdl_path]
+#
 if [ $BASH_SOURCE != $0 ] ; then
    echo "Please run this script throw one in the 2 next commands:"
    echo "1/    ./quick_run_GDL.sh"
@@ -16,12 +17,15 @@ fi
 if [ -z $flag_help ] ;     then flag_help=0 ; fi
 if [ -z $flag_quiet ] ;    then flag_quiet=0 ; fi
 if [ -z $flag_valgrind ] ; then flag_valgrind=0 ; fi
+if [ -z $flag_gdl_path ] ; then flag_gdl_path=0 ; fi
 # 
-while getopts ":hqv-:" opt ; do
+while getopts ":ghqv-:" opt ; do
     case $opt in 
 	- ) case $OPTARG in
 	    quiet* )    echo "Quiet keyword swith ON"
 	                flag_quiet=1 ;;
+	    gdl_path)   echo "Keeping GDL_PATH swith ON"
+	                flag_gdl_path=1 ;;
 	    help* )     echo "Help keyword swith ON"
                         flag_help=1 ;;
 	    valgrind* ) echo "Valgrind keyword swith ON"
@@ -31,8 +35,10 @@ while getopts ":hqv-:" opt ; do
 	    * )         echo "option illegal -$OPTARG
 	                exit
 	esac ;;
-    q )   echo "Quiet keyword swith ON"
-          flag_quiet=1 ;;
+    q )  echo "Quiet keyword swith ON"
+         flag_quiet=1 ;;
+    g )  echo "Keeping GDL_PATH swith ON"
+         flag_gdl_path=1 ;; 
     h )  echo "Help keyword swith ON"
          flag_help=1 ;;
     v* ) echo "Valgrind keyword swith ON"
@@ -43,7 +49,7 @@ while getopts ":hqv-:" opt ; do
 done
 #
 if [ "$flag_help" -eq 1 ] ; then
-    echo "Usage: gdl -h [--help] -q [--quiet] -v [--valgrind]"
+    echo "Usage: gdl -g [--gdl_path] -h [--help] -q [--quiet] -v [--valgrind]"
     exit
 fi
 #
@@ -51,7 +57,12 @@ if [ "$flag_quiet" -eq 0 ] ; then
     echo "Running current GDL with ./src/pro and ./testsuite in GDL_PATH"
 fi
 #
-export GDL_PATH='+'${PWD}'/src/pro/:+'${PWD}'/testsuite/'
+if [ "$flag_gdl_path" -eq 0 ] ; then
+    export GDL_PATH='+'${PWD}'/src/pro/:+'${PWD}'/testsuite/'
+else
+    export GDL_PATH='+'${PWD}'/src/pro/:+'${PWD}'/testsuite/:'${GDL_PATH}    
+fi
+#
 if [ "$flag_quiet" -eq 0 ] ; then
     echo $GDL_PATH
 fi
