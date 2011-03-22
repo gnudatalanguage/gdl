72abff89d59985cf156acfd3bff82159284589fd
Mark Schellens <mschellens@gmail.com>
2011-03-22 20:45:17 +0000

    Included patch for udunits2. Bug tracker ID 3233853

--- src/gsl_fun.cpp
+++ src/gsl_fun.cpp
@@ -49,7 +49,8 @@
 #include <gsl/gsl_const_num.h>
 #include <gsl/gsl_math.h>
 #ifdef USE_UDUNITS
-#  include <udunits2.h>
+//#  include <udunits2.h>
+#  include <udunits2/udunits2.h>
 #endif
 
 // binomialcoef
