0a96d539e8eb85275de931be5ccf9d8bf6053f52
Sylwester Arabas <slayoo@igf.fuw.edu.pl>
2011-03-30 01:28:49 +0000

    cmake: patch by Orion to get netcdf working with centos 5

--- CMakeModules/FindNetCDF.cmake
+++ CMakeModules/FindNetCDF.cmake
@@ -5,18 +5,17 @@ if(NETCDFPKG)
 	include(FindPkgConfig)
 	pkg_check_modules(NETCDF netcdf)
 	if(NETCDF_FOUND)
-		set(NETCDF_INCLUDE_DIR ${NETCDF_INCLUDE_DIRS})
 		find_package_handle_standard_args(NETCDF DEFAULT_MSG NETCDF_LIBRARIES)
 	endif(NETCDF_FOUND)
 else(NETCDFPKG) # no netcdf.pc file
 	find_library(NETCDF_LIBRARIES NAMES netcdf)
-	find_path(NETCDF_INCLUDE_DIR NAMES netcdfcpp.h)	
-	find_package_handle_standard_args(NETCDF DEFAULT_MSG NETCDF_LIBRARIES NETCDF_INCLUDE_DIR)
+	find_path(NETCDF_INCLUDE_DIRS NAMES netcdfcpp.h PATH_SUFFIXES netcdf-3)	
+	find_package_handle_standard_args(NETCDF DEFAULT_MSG NETCDF_LIBRARIES NETCDF_INCLUDE_DIRS)
 endif(NETCDFPKG)
 
 mark_as_advanced(
 NETCDFPKG
 NETCDF
-NETCDF_INCLUDE_DIR
+NETCDF_INCLUDE_DIRS
 NETCDF_LIBRARIES
 )
