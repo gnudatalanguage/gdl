3a3198be8a926766e48ca0147d99675675094a29
Sylwester Arabas <slayoo@igf.fuw.edu.pl>
2011-11-09 09:33:23 +0000

    www: news about 0.9.2 release and ADASS XXI

--- doc/www/_news.inc.php
+++ doc/www/_news.inc.php
@@ -1,3 +1,12 @@
+<h2>GDL 0.9.2 released</h2>
+<p>
+  2011-11-09: GDL 0.9.2 source tarball released.
+  <span class="align-right"><a href="http://sourceforge.net/projects/gnudatalanguage/files/gdl/0.9.2/README/download">release notes</a> | <a href="downloads.php">downloads</a></span>
+</p>
+
+<h2>GDL @ADASS XXI</h2>
+<p>2011-11-09: GDL talk at the <a href="http://www.eso.org/sci/meetings/2011/adass2011.html">ADASS XXI</a> in Paris</p>
+
 <h2>GDL @GHM2011</h2>
 <p>2011-08-27: <a href="http://www.igf.fuw.edu.pl/~slayoo/gdl-ghm2011paris.pdf">GDL talk</a> at the <a href="http://www.gnu.org/ghm/2011/paris/">GHM</a> in Paris</p>
 
