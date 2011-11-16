8d82d90ff2dfcd231f18aaa91886b0a698714173
Sylwester Arabas <slayoo@igf.fuw.edu.pl>
2011-11-16 16:25:27 +0000

    doc/www: news updates

--- doc/www/_news.inc.php
+++ doc/www/_news.inc.php
@@ -1,6 +1,9 @@
 <h2>GDL 0.9.2 released</h2>
 <p>
+  2011-11-16: Fink package updated<br />
+  2011-11-15: Debian package updated<br />
   2011-11-14: Macports port updated<br />
+  2011-11-13: HMUG package updated<br />
   2011-11-13: FreeBSD port updated<br />
   2011-11-12: ArchLinux package updated<br />
   2011-11-11: Fedora package updated<br />
@@ -9,7 +12,7 @@
 </p>
 
 <h2>GDL @ADASS XXI</h2>
-<p>2011-11-09: GDL talk at the <a href="http://www.eso.org/sci/meetings/2011/adass2011.html">ADASS XXI</a> in Paris</p>
+<p>2011-11-09: <a href="http://aramis.obspm.fr/~coulais/IDL_et_GDL/Adass2011/CoulaisADASS2011.pdf">GDL talk</a> at the <a href="http://www.eso.org/sci/meetings/2011/adass2011.html">ADASS XXI</a> in Paris</p>
 
 <h2>GDL @GHM2011</h2>
 <p>2011-08-27: <a href="http://www.igf.fuw.edu.pl/~slayoo/gdl-ghm2011paris.pdf">GDL talk</a> at the <a href="http://www.gnu.org/ghm/2011/paris/">GHM</a> in Paris</p>
