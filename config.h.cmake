6c83a649e5083ebe6741a859ccdb880d14378163
Alain Coulais <alain.coulais@obspm.fr>
2012-11-12 14:04:27 +0000

    tuning HAVE_PLPLOT_BEFORE_5994 to have success on more than Debian ;-)

--- config.h.cmake
+++ config.h.cmake
@@ -27,6 +27,7 @@
 #cmakedefine HAVE_MEMORY_H 1
 #cmakedefine HAVE_NEXTTOWARD 1
 #cmakedefine HAVE_OLDPLPLOT 1
+#cmakedefine HAVE_PLPLOT_BEFORE_5994 1
 #ifndef HAVE_STDINT_H
 #cmakedefine HAVE_STDINT_H 1
 #endif
