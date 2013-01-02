afaeed35a220ecac41399a181d2c1f296dca7da6
Gilles Duvert <gilles.duvert@univ-grenoble-alpes.fr>
2013-01-02 23:26:23 +0000

    needed to find libps on some 64 bits architectures

--- CMakeModules/Findlibps.cmake
+++ CMakeModules/Findlibps.cmake
@@ -1,4 +1,4 @@
-find_path(LIBPSPKG libps.pc PATH_SUFFIXES lib lib/pkgconfig)
+find_path(LIBPSPKG libps.pc PATH_SUFFIXES lib lib/pkgconfig lib64/pkgconfig)
 include(FindPackageHandleStandardArgs)
 if(LIBPSPKG)
 	set(ENV{PKG_CONFIG_PATH} ${LIBPSPKG}) # pkg search path
