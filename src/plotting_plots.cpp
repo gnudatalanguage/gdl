8daea8a1b6b43627d4c72ea71a3d1215481f0a87
Gilles Duvert <gilles.duvert@univ-grenoble-alpes.fr>
2013-01-03 17:22:33 +0000

    All keywords supported except Z and T3D Provides /CONTINUE Correct handling of log/DATA/NORMAL/DEVICE coordinates CLIP ok.

--- src/plotting_plots.cpp
+++ src/plotting_plots.cpp
@@ -19,269 +19,301 @@
 #include "plotting.hpp"
 #include "math_utl.hpp"
 
-namespace lib {
+namespace lib
+{
 
   using namespace std;
 
-  class plots_call : public plotting_routine_call
+  class plots_call: public plotting_routine_call
   {
 
     DDoubleGDL *xVal, *yVal, *zVal;
     auto_ptr<BaseGDL> xval_guard, yval_guard;
     DLong psym;
-    PLFLT xStart, xEnd, yStart, yEnd, zStart, zEnd;
-    PLFLT xMarginL, xMarginR, yMarginB, yMarginT;
-    bool xLog, yLog, xLogOrig, yLogOrig;
+    bool xLog, yLog, zLog;
     SizeT xEl, yEl, zEl;
     bool append;
+    bool doClip;
+    bool restoreClipBox;
+    PLFLT savebox[4];
 
-    private: bool handle_args(EnvT* e) // {{{
+  private:
+
+    bool handle_args(EnvT* e) // {{{
     {
       append=e->KeywordSet("CONTINUE");
-      if( nParam() == 1)
+      if ( nParam()==1 )
       {
         BaseGDL* p0;
-        p0 = e->GetParDefined( 0);  
-        if (p0->Dim(0) != 2)
-          e->Throw( "When only 1 param, dims must be (2,n)");
+        p0=e->GetParDefined(0);
+        if ( p0->Dim(0)!=2 )
+          e->Throw("When only 1 param, dims must be (2,n)");
 
-        if (e->KeywordSet("T3D")) e->Throw("Only 3-argument case supported for T3D - FIXME!");
+        if ( e->KeywordSet("T3D") ) e->Throw("Only 3-argument case supported for T3D - FIXME!");
         // TODO: the (3,n) case
 
-        DDoubleGDL *val = e->GetParAs< DDoubleGDL>( 0);
+        DDoubleGDL *val=e->GetParAs< DDoubleGDL>(0);
 
-        xEl = p0->N_Elements() / p0->Dim(0);
-        xVal = new DDoubleGDL( dimension( xEl), BaseGDL::NOZERO);
-        xval_guard.reset( xVal); // delete upon exit
+        xEl=p0->N_Elements()/p0->Dim(0);
+        xVal=new DDoubleGDL(dimension(xEl), BaseGDL::NOZERO);
+        xval_guard.reset(xVal); // delete upon exit
 
-        yEl = p0->N_Elements() / p0->Dim(0);
-        yVal = new DDoubleGDL( dimension( yEl), BaseGDL::NOZERO);
-        yval_guard.reset( yVal); // delete upon exit
+        yEl=p0->N_Elements()/p0->Dim(0);
+        yVal=new DDoubleGDL(dimension(yEl), BaseGDL::NOZERO);
+        yval_guard.reset(yVal); // delete upon exit
 
-        for( SizeT i = 0; i < xEl; i++) 
+        for ( SizeT i=0; i<xEl; i++ )
         {
-	  (*xVal)[i] = (*val)[2*i];
-	  (*yVal)[i] = (*val)[2*i+1];
+          (*xVal)[i]=(*val)[2*i];
+          (*yVal)[i]=(*val)[2*i+1];
         }
       }
-      else if(nParam() == 2)
+      else if ( nParam()==2 )
       {
-        if (e->KeywordSet("T3D")) e->Throw("Only 3-argument case supported for T3D - FIXME!"); // TODO
+        if ( e->KeywordSet("T3D") ) e->Throw("Only 3-argument case supported for T3D - FIXME!"); // TODO
 
-        xVal = e->GetParAs< DDoubleGDL>( 0);
-        xEl = xVal->N_Elements();
+        xVal=e->GetParAs< DDoubleGDL>(0);
+        xEl=xVal->N_Elements();
 
-        yVal = e->GetParAs< DDoubleGDL>( 1);
-        yEl = yVal->N_Elements();
+        yVal=e->GetParAs< DDoubleGDL>(1);
+        yEl=yVal->N_Elements();
         //silently drop unmatched values
-        if (yEl!=xEl)
+        if ( yEl!=xEl )
         {
           SizeT size;
-          size=min(xEl,yEl);
+          size=min(xEl, yEl);
           xEl=size;
           yEl=size;
         }
       }
-      else if(nParam() == 3)
+      else if ( nParam()==3 )
       {
-        zVal = e->GetParAs< DDoubleGDL>( 2);
-        zEl = zVal->N_Elements();
+        zVal=e->GetParAs< DDoubleGDL>(2);
+        zEl=zVal->N_Elements();
 
         //if ((*zVal)[0] == 0 && zEl == 1) {
-          xVal = e->GetParAs< DDoubleGDL>( 0);
-          xEl = xVal->N_Elements();
+        xVal=e->GetParAs< DDoubleGDL>(0);
+        xEl=xVal->N_Elements();
 
-          yVal = e->GetParAs< DDoubleGDL>( 1);
-          yEl = yVal->N_Elements();
-        if (!(xEl==yEl && yEl==zEl))
+        yVal=e->GetParAs< DDoubleGDL>(1);
+        yEl=yVal->N_Elements();
+        if ( !(xEl==yEl&&yEl==zEl) )
         {
           SizeT size;
-          size=min(xEl,yEl);
-          size=min(size,zEl);
+          size=min(xEl, yEl);
+          size=min(size, zEl);
           xEl=size;
           yEl=size;
           zEl=size;
         }
       }
       return false;
-    } 
+    }
 
-  private: void old_body( EnvT* e, GDLGStream* actStream) // {{{
-  {
-    DDoubleGDL* clippingD=NULL;
+  private:
 
-    actStream->gvpd(xMarginL,xMarginR,yMarginB,yMarginT);
-    if((xMarginL==0.0&&xMarginR==0.0)||(yMarginB==0.0&&yMarginT==0.0)) //if not initialized, set normalized mode
+    void old_body(EnvT* e, GDLGStream* actStream)
     {
-        actStream->NoSub();
-        actStream->vpor(0, 1, 0, 1);
-        actStream->gvpd(xMarginL,xMarginR,yMarginB,yMarginT);
-        actStream->wind(0.0,1.0,0.0,1.0);
-   }
-    // get current viewport limit in world coords
-//    get_axis_crange("X",xStart,xEnd);
-//    get_axis_crange("Y",yStart,yEnd);
-    actStream->gvpw(xStart,xEnd,yStart,yEnd);
-    // get_axis_type
-    get_axis_type("X", xLogOrig); xLog=xLogOrig;
-    get_axis_type("Y", yLogOrig); yLog=yLogOrig;
-    
-    /*    DLong background = p_background;
-    static int cix=e->KeywordIx("COLOR");
-    BaseGDL* color_arr=e->GetKW(cix);
-    DLongGDL* l_color_arr;
+      int clippingix=e->KeywordIx("CLIP");
+      DFloatGDL* clipBox=NULL;
 
-    if(color_arr != NULL)
+      enum
       {
-	l_color_arr=static_cast<DLongGDL*>
-	  (color_arr->Convert2(GDL_LONG, BaseGDL::COPY));
-	if(color_arr->N_Elements() < minEl && color_arr->N_Elements() > 1)
-	  e->Throw( "Array "+e->GetParString(cix)+
-		    " does not have enough elements for COLOR keyword.");
-      }
-    DLong color = p_color;
+        DATA=0,
+        NORMAL,
+        DEVICE
+      } coordinateSystem=DATA;
+      //check presence of DATA,DEVICE and NORMAL options
+      if ( e->KeywordSet("DATA") ) coordinateSystem=DATA;
+      if ( e->KeywordSet("DEVICE") ) coordinateSystem=DEVICE;
+      if ( e->KeywordSet("NORMAL") ) coordinateSystem=NORMAL;
+      // get_axis_type
+      gdlGetAxisType("X", xLog);
+      gdlGetAxisType("Y", yLog);
+      gdlGetAxisType("Z", zLog);
 
-    if(color_arr != NULL)  
-      if(color_arr->N_Elements() >= 1) 
-	  	color=(*l_color_arr)[0];
-    */
-  
-    bool mapSet=false;
+      bool mapSet=false;
 #ifdef USE_LIBPROJ4
-    // Map Stuff (xtype = 3)
-    LPTYPE idata;
-    XYTYPE odata;
+      // Map Stuff (xtype = 3)
 
-    get_mapset(mapSet);
+      get_mapset(mapSet);
 
-    if ( mapSet) {
-      ref = map_init();
-      if ( ref == NULL) {
-	e->Throw( "Projection initialization failed.");
+      if ( mapSet )
+      {
+        ref=map_init();
+        if ( ref==NULL )
+        {
+          e->Throw("Projection initialization failed.");
+        }
       }
-    }
 #endif
-    //CLIPPING
-    DLong noclip = 1; //PLOTS: default is not to clip, even when clip= is present. Only with noclip=0 a clip is done.
-    e->AssureLongScalarKWIfPresent( "NOCLIP", noclip);
-    if( noclip == 0)
-    {
-      static int clippingix = e->KeywordIx( "CLIP");
-      clippingD = e->IfDefGetKWAs<DDoubleGDL>( clippingix);
+      restoreClipBox=false;
+      int noclipvalue=1;
+      e->AssureLongScalarKWIfPresent( "NOCLIP", noclipvalue);
+      doClip=(noclipvalue==0); //PLOTS by default does not clip, even if clip is defined by CLIP= or !P.CLIP
+      clipBox=e->IfDefGetKWAs<DFloatGDL>(clippingix);
+      if (doClip && clipBox!=NULL && clipBox->N_Elements()>=4 ) //clipbox exist, will be used: convert to device coords
+                                   //and save in !P.CLIP...
+      {
+        restoreClipBox=true; //restore later
+        // save current !P.CLIP box, replace by our current clipbox in whatever coordinates, will
+        // give back the !P.CLIP box at end...
+        static DStructGDL* pStruct=SysVar::P();
+        static unsigned clipTag=pStruct->Desc()->TagIndex("CLIP"); //must be in device coordinates
+        static PLFLT tempbox[4];
+        for ( int i=0; i<4; ++i ) savebox[i]=(*static_cast<DLongGDL*>(pStruct->GetTag(clipTag, 0)))[i];
+        if ( coordinateSystem==DEVICE )
+        {
+          for ( int i=0; i<4; ++i ) tempbox[i]=(*clipBox)[i];
+        }
+        else if ( coordinateSystem==DATA )
+        {
+          //handle log: if existing box is already in log, use log of clipbox values.
+          PLFLT worldbox[4];
+          for ( int i=0; i<4; ++i ) worldbox[i]=(*clipBox)[i];
+          if (xLog) {worldbox[0]=log10(worldbox[0]); worldbox[2]=log10(worldbox[2]);}
+          if (yLog) {worldbox[1]=log10(worldbox[1]); worldbox[3]=log10(worldbox[3]);}
+          bool okClipBox=true;
+          for ( int i=0; i<4; ++i )
+          {
+            if (!(worldbox[i]==worldbox[i])) //NaN
+            {
+              okClipBox=false;restoreClipBox=false;doClip=false;
+            }
+          }
+          if (okClipBox)
+          {
+            actStream->WorldToDevice(worldbox[0], worldbox[1], tempbox[0], tempbox[1]);
+            actStream->WorldToDevice(worldbox[2], worldbox[3], tempbox[2], tempbox[3]);
+          }
+        }
+        else
+        {
+          actStream->NormedDeviceToDevice((*clipBox)[0],(*clipBox)[1], tempbox[0], tempbox[1]);
+          actStream->NormedDeviceToDevice((*clipBox)[2],(*clipBox)[3], tempbox[2], tempbox[3]);
+        }
+        //place in !P.CLIP
+        for ( int i=0; i<4; ++i ) (*static_cast<DLongGDL*>(pStruct->GetTag(clipTag, 0)))[i]=tempbox[i];
+      }
+
+      PLFLT wun, wdeux, wtrois, wquatre;
+      if ( coordinateSystem==DATA) //with PLOTS, we can plot *outside* the box(e)s in DATA coordinates.
+                                   // convert to device coords in this case
+      {
+        actStream->pageWorldCoordinates(wun, wdeux, wtrois, wquatre);
+      }
+
+      actStream->OnePageSaveLayout(); // one page
+
+      if ( !e->KeywordSet("T3D") )
+      {
+        actStream->vpor(0, 1, 0, 1);
+        if ( coordinateSystem==DEVICE )
+        {
+          actStream->wind(0.0, actStream->xPageSize(), 0.0, actStream->yPageSize());
+          xLog=false;
+          yLog=false;
+        }
+        else if ( coordinateSystem==NORMAL )
+        {
+          actStream->wind(0, 1, 0, 1);
+          xLog=false;
+          yLog=false;
+        }
+        else //with PLOTS, we can plot *outside* the box(e)s in DATA coordinates.
+        {
+          actStream->wind(wun, wdeux, wtrois, wquatre);
+        }
+      }
     }
 
-    if (!e->KeywordSet("T3D"))
+  private:
+
+    void call_plplot(EnvT* e, GDLGStream* actStream) 
     {
-        actStream->NoSub();
-        if (e->KeywordSet("DEVICE"))
-      {
-        actStream->vpor(0, 1, 0, 1);
-        PLFLT xpix, ypix;
-        PLFLT un,deux,trois,quatre;
-        PLINT xleng, yleng, xoff, yoff;
-        actStream->gpage(xpix, ypix, xleng, yleng, xoff, yoff);
-        un=0.0; deux=xleng; trois=0.0; quatre=yleng;
-//        if( clippingD != NULL) Clipping( clippingD, un, deux, trois, quatre);
-        actStream->wind(un, deux, trois, quatre);
-        xLog = false;
-        yLog = false;
-      }
-      else if (e->KeywordSet("NORMAL"))
-      {
-        PLFLT un,deux,trois,quatre;
-        actStream->vpor(0, 1, 0, 1);
-        un=0.0; deux=1.0; trois=0.0; quatre=1.0;
- //       if( clippingD != NULL) Clipping( clippingD, un, deux, trois, quatre);
-        actStream->wind(un, deux, trois, quatre);
-        xLog = false;
-        yLog = false;
-      }
-//      else if( clippingD != NULL)
-//      {
-//         PLFLT un,deux,trois,quatre;
-//         un=xStart; deux=xEnd; trois=yStart; quatre=yEnd;
-//         Clipping( clippingD, un, deux, trois, quatre);
-//         actStream->wind(un, deux, trois, quatre);
-//       }
-      else //with PLOTS, we can plot *outside* the box(e)s in DATA coordinates.
-      {
-        DDouble un, deux, trois, quatre;
-        getWorldCoordinatesFromPLPLOT(actStream, 0.0, 0.0, &un, &trois);
-        getWorldCoordinatesFromPLPLOT(actStream, 1.0, 1.0, &deux, &quatre);
-
-        actStream->vpor(0, 1, 0, 1);
-        actStream->wind((PLFLT)un, (PLFLT)deux, (PLFLT)trois, (PLFLT)quatre);
-      }
-   }
-    // start drawing. Graphic Keywords accepted: CLIP(YES), COLOR(OK), DATA(YES), DEVICE(YES),
-    //LINESTYLE(OK), NOCLIP(YES), NORMAL(YES), PSYM(OK), SYMSIZE(OK), T3D(NO), THICK(OK), Z(NO)
-    gkw_color(e, actStream); //COLOR
-    gkw_psym(e, psym); //PSYM
-    gkw_linestyle(e, actStream); //LINESTYLE
-    gkw_symsize(e, actStream); //SYMSIZE
-    gkw_thick(e, actStream); //THICK
-
-  } 
-
-    private: void call_plplot(EnvT* e, GDLGStream* actStream) // {{{
-    {
-      if (e->KeywordSet("T3D")) // TODO: check !P.T3D
+      // start drawing. Graphic Keywords accepted: CLIP(YES), COLOR(OK), DATA(YES), DEVICE(YES),
+      //LINESTYLE(OK), NOCLIP(YES), NORMAL(YES), PSYM(OK), SYMSIZE(OK), T3D(NO), THICK(OK), Z(NO)
+      if ( e->KeywordSet("T3D") ) // TODO: check !P.T3D
       {
         Warning("PLOTS: 3D plotting does not really work yet (!P.T and !P.T3D are ignored for the moment)");
- /*       actStream->w3d(1.2, 1.2, 2.2, // TODO!
-          xStart, xEnd, yStart, yEnd, xStart, zEnd,
-          30, 30  // TODO!
-        );
-*/
-        PLINT n = xVal->N_Elements();
-        if (yEl > 1 && yEl < n) n = yEl;
-        if (zEl > 1 && zEl < n) n = zEl;
 
-        PLFLT *x = new PLFLT[n];
+        /*       actStream->w3d(1.2, 1.2, 2.2, // TODO!
+                 xStart, xEnd, yStart, yEnd, xStart, zEnd,
+                 30, 30  // TODO!
+               );
+         */
+
+        gdlSetGraphicsForegroundColorFromKw(e, actStream); //COLOR
+        gdlGetPsym(e, psym); //PSYM
+        gdlSetLineStyle(e, actStream); //LINESTYLE
+        gdlSetSymsize(e, actStream); //SYMSIZE
+        gdlSetPenThickness(e, actStream); //THICK
+
+        PLINT n=xVal->N_Elements();
+        if ( yEl>1&&yEl<n ) n=yEl;
+        if ( zEl>1&&zEl<n ) n=zEl;
+        ArrayGuard<PLFLT> xGuard, yGuard, zGuard;
+        PLFLT *x=new PLFLT[n];
+        xGuard.Reset(x);
         {
-          int a = (xEl != 1); 
-          for (PLINT i = 0; i < n; ++i) x[i] = (*xVal)[a * i];
+          int a=(xEl!=1);
+          for ( PLINT i=0; i<n; ++i ) x[i]=(xLog)?(*xVal)[a*i]:log10((*xVal)[a*i]);
         }
 
-        PLFLT *y = new PLFLT[n];
+        PLFLT *y=new PLFLT[n];
+        yGuard.Reset(y);
         {
-          int a = (yEl != 1); 
-          for (PLINT i = 0; i < n; ++i) y[i] = (*yVal)[a * i];
+          int a=(yEl!=1);
+          for ( PLINT i=0; i<n; ++i ) y[i]=(yLog)?(*yVal)[a*i]:log10((*yVal)[a*i]);
         }
 
-        PLFLT *z = new PLFLT[n];
+        PLFLT *z=new PLFLT[n];
+        zGuard.Reset(z);
         {
-          int a = (zEl != 1); 
-          for (PLINT i = 0; i < n; ++i) z[i] = (*zVal)[a * i];
+          int a=(zEl!=1);
+          for ( PLINT i=0; i<n; ++i ) z[i]=(zLog)?(*zVal)[a*i]:log10((*zVal)[a*i]);
         }
 
         actStream->line3(n, x, y, z);
-
-        delete[] x;
-        delete[] y;
-        delete[] z;
       }
-      else 
+      else
       {
-        // there is no 'minmax' option for plots. 
-        bool valid = draw_polyline(e, actStream, xVal, yVal, 0.0, 0.0, false, xLog, yLog, psym, append);
+        // make all clipping computations BEFORE setting graphic properties (color, size)
+        bool stopClip=false;
+        if ( doClip )  if ( startClipping(e, actStream, true)==TRUE ) stopClip=true;
+        //properties
+        gdlSetGraphicsForegroundColorFromKw(e, actStream); //COLOR
+        gdlGetPsym(e, psym); //PSYM
+        gdlSetLineStyle(e, actStream); //LINESTYLE
+        gdlSetSymsize(e, actStream); //SYMSIZE
+        gdlSetPenThickness(e, actStream); //THICK
+
+        bool valid=draw_polyline(e, actStream, xVal, yVal, 0.0, 0.0, false, xLog, yLog, psym, append);
         // TODO: handle valid?
+        if (stopClip) stopClipping(actStream);
       }
-    } 
+    }
 
-    private: virtual void post_call(EnvT*, GDLGStream* actStream) 
+  private:
+
+    virtual void post_call(EnvT*, GDLGStream* actStream)
     {
-      actStream->lsty(1);//reset linestyle
-      actStream->vpor(xMarginL, xMarginR, yMarginB, yMarginT);
-      actStream->wind(xStart, xEnd, yStart, yEnd);
-    } 
+      actStream->RestoreLayout();
+      actStream->lsty(1); //reset linestyle
+      if (restoreClipBox)
+      {
+        static DStructGDL* pStruct=SysVar::P();
+        static unsigned clipTag=pStruct->Desc()->TagIndex("CLIP"); //must be in device coordinates
+        for ( int i=0; i<4; ++i ) (*static_cast<DLongGDL*>(pStruct->GetTag(clipTag, 0)))[i]=savebox[i];
+      }
+    }
 
-  }; // oplot_call class 
+  }; 
 
   void plots(EnvT* e)
   {
     plots_call plots;
     plots.call(e, 1);
-  }  
+  }
 
 } // namespace
