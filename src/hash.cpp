6621a158b0c0f0bb4727716efdaa69c5e31b2cca
Mark Schellens <mschellens@gmail.com>
2013-07-15 13:44:56 +0000

    add files for HASH

---
+++ src/hash.cpp
@@ -0,0 +1,32 @@
+/***************************************************************************
+                          hash.cpp  - for HASH objects
+                             -------------------
+    begin                : July 22 2013
+    copyright            : (C) 2013 by M. Schellens et al.
+    email                : m_schellens@users.sf.net
+
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "includefirst.hpp"
+
+#include "nullgdl.hpp"
+#include "datatypes.hpp"
+#include "envt.hpp"
+#include "dpro.hpp"
+#include "dinterpreter.hpp"
+  
+  
+namespace lib {
+
+  
+  
+} // namespace lib
---
+++ src/hash.hpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+                          hash.hpp  - for HASH objects
+                             -------------------
+    begin                : July 22 2013
+    copyright            : (C) 2013 by M. Schellens et al.
+    email                : m_schellens@users.sf.net
+
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef HASH_HPP_
+#define HASH_HPP_
+
+
+namespace lib {
+
+
+}
+
+#endif
