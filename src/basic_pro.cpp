6534cd4ecd9f02aea7d3ff1b0f217b08d1d0d25f
Mark Schellens <mschellens@gmail.com>
2013-10-06 03:10:04 +0000

    for windows

--- src/basic_pro.cpp
+++ src/basic_pro.cpp
@@ -20,7 +20,6 @@
 #include "includefirst.hpp"
 
 #include <sys/types.h>
-#include <dirent.h>
 
 #include <string>
 #include <fstream>
@@ -32,6 +31,48 @@
 #include <sys/types.h>
 #ifndef _MSC_VER
 #include <sys/wait.h>
+#endif
+
+#ifndef _MSC_VER
+#	include <dirent.h>
+#else
+  // MSC workaround implementation in file.cpp
+  /*
+      Declaration of POSIX directory browsing functions and types for Win32.
+
+      Author:  Kevlin Henney (kevlin@acm.org, kevlin@curbralan.com)
+      History: Created March 1997. Updated June 2003.
+      Rights:  See end of file.
+  */
+  extern "C"
+  {
+
+  typedef struct DIR DIR;
+
+  struct dirent
+  {
+      char *d_name;
+  };
+
+  DIR           *opendir(const char *);
+  int           closedir(DIR *);
+  struct dirent *readdir(DIR *);
+  void          rewinddir(DIR *);
+
+  /*
+      Copyright Kevlin Henney, 1997, 2003. All rights reserved.
+
+      Permission to use, copy, modify, and distribute this software and its
+      documentation for any purpose is hereby granted without fee, provided
+      that this copyright and permissions notice appear in all copies and
+      derivatives.
+      
+      This software is supplied "as is" without express or implied warranty.
+
+      But that said, if there are any problems please get in touch.
+  */
+
+  } // extern "C"
 #endif
 
 #ifdef __APPLE__
