4a5920a65e0cce985a53dc1b4fd8cc5dd92c19f7
Alain Coulais <alain.coulais@obspm.fr>
2013-10-11 12:35:19 +0000

    continuing adding news (link to adass paper)

--- doc/www/_news.inc.php
+++ doc/www/_news.inc.php
@@ -2,9 +2,14 @@
 <p>
   2013-10-11: <a href="http://www.freshports.org/science/gnudatalanguage/">FreeBSD port updated</a><br />
   2013-10-02: <a href="https://trac.macports.org/changeset/111797">MacPort updated</a><br />
-  2013-09-29: <a href="http://adass2013.cfht.hawaii.edu/presenterdetails.php?id=189">poster</a> at <a href="http://adass2013.cfht.hawaii.edu/index.php">ADASS 2013</a> 
   2013-09-28: GDL 0.9.4 source tarball released.<br />
  <span class="align-right"><a href="http://sourceforge.net/projects/gnudatalanguage/files/gdl/0.9.4/README/download">release notes</a> | <a href="downloads.php">downloads</a></span>
+</p>
+
+<h2>GDL @ADASS XXIII</h2>
+<p>  
+  2013-09-29: <a href="http://adass2013.cfht.hawaii.edu/presenterdetails.php?id=189">poster</a> at <a href="http://adass2013.cfht.hawaii.edu/index.php">ADASS 2013</a> 
+  2013-09-29: <a href="http://aramis.obspm.fr/~coulais/IDL_et_GDL/Adass2013/P39_Adass2013_Coulais.pdf">Draft of the paper (pdf)</a>
 </p>
 
 <h2>GDL 0.9.3 released</h2>
--- doc/www/resources.php
+++ doc/www/resources.php
@@ -10,7 +10,9 @@
   There were several talks and posters on GDL prepared by the project team:
 </p>
 <ul>
-  <li><a href="http://aramis.obspm.fr/~coulais/IDL_et_GDL/Adass2011/slides_ADASS2011_GDL_Coulais.pdf">talk @ ADASS 2011</a> by Alain Coulais</li> (no definitive version of the <a href="http://aramis.obspm.fr/~coulais/IDL_et_GDL/Adass2011/O11_ADASS2011_GDL_Coulais.pdf">corresponding paper</a>)
+  <li><a href="http://adass2013.cfht.hawaii.edu/presenterdetails.php?id=189">poster</a> @ <a href="http://adass2013.cfht.hawaii.edu/index.php">ADASS 2013</a>by Alain Coulais et al.</li>
+(no definitive version of the <a href="http://aramis.obspm.fr/~coulais/IDL_et_GDL/Adass2013/P39_Adass2013_Coulais.pdf">corresponding paper</a> [in pdf])
+  <li><a href="http://aramis.obspm.fr/~coulais/IDL_et_GDL/Adass2011/slides_ADASS2011_GDL_Coulais.pdf">talk @ ADASS 2011</a> by Alain Coulais</li> (no definitive version of the <a href="http://aramis.obspm.fr/~coulais/IDL_et_GDL/Adass2011/O11_ADASS2011_GDL_Coulais.pdf">corresponding paper</a> [in pdf])
   <li><a href="http://www.igf.fuw.edu.pl/~slayoo/gdl-ghm2011paris.pdf">talk @ GHM 2011</a> by Alain Coulais &amp; Sylwester Arabas</li>
   <li><a href="http://www.igf.fuw.edu.pl/~slayoo/gdl-fosdem-2011-talk.pdf">talk @ FOSDEM 2011</a> by Sylwester Arabas</li>
   <li><a href="http://www.igf.fuw.edu.pl/~slayoo/prezentacja-jamstec-20100820.pdf">talk/tutorial at The Earth Simulator Center</a> by Alain Coulais, Sylwester Arabas &amp; Takeshi Enomoto</li>
