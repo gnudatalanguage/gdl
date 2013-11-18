e0a81fb2b5881105181343b20f0070cb8a0f3bf8
Alain Coulais <alain.coulais@obspm.fr>
2013-11-18 10:53:50 +0000

    in !cpu, two fields now in Long64

--- src/initsysvar.cpp
+++ src/initsysvar.cpp
@@ -704,8 +704,8 @@ namespace SysVar
     cpuData->NewTag("HW_NCPU", new DLongGDL( 1)); 
 #endif
     cpuData->NewTag("TPOOL_NTHREADS", new DLongGDL( CpuTPOOL_NTHREADS)); 
-    cpuData->NewTag("TPOOL_MIN_ELTS", new DLongGDL( CpuTPOOL_MIN_ELTS)); 
-    cpuData->NewTag("TPOOL_MAX_ELTS", new DLongGDL( CpuTPOOL_MAX_ELTS)); 
+    cpuData->NewTag("TPOOL_MIN_ELTS", new DLong64GDL( CpuTPOOL_MIN_ELTS)); 
+    cpuData->NewTag("TPOOL_MAX_ELTS", new DLong64GDL( CpuTPOOL_MAX_ELTS)); 
 
     DVar *cpu=new DVar( "CPU", cpuData);
     cpuIx=sysVarList.size();
