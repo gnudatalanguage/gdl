bfdf34941dac3d23f7de2bd2c05a3926b16b6c2b
Gilles Duvert <gilles.duvert@univ-grenoble-alpes.fr>
2014-09-08 21:39:16 +0000

    moved get_screen_size to plotting_misc + patches by GJ

--- src/plotting_windows.cpp
+++ src/plotting_windows.cpp
@@ -18,12 +18,6 @@
 #include "includefirst.hpp"
 #include "plotting.hpp"
 
-// patch by Greg Jung
-#if defined(_WIN32) && !defined(__CYGWIN__)
-#  include "devicewin.hpp"
-#else
-#  include "devicex.hpp"
-#endif
 
 namespace lib {
 
@@ -71,11 +65,9 @@ namespace lib {
     e->AssureLongScalarKWIfPresent( "YPOS", yPos);
 
     DLong xSize, ySize;
-#ifdef HAVE_X
-    DeviceX::DefaultXYSize(&xSize, &ySize);
-#else
-    xSize = 640;
-    ySize = 512;
+
+    actDevice->DefaultXYSize(&xSize, &ySize);
+
 #ifdef _MSC_VER
     // the following not tested (no access to windows) (GD)
     bool noQscreen=true;
@@ -86,7 +78,7 @@ namespace lib {
     string gdlYsize=GetEnvString("GDL_GR_WIN_HEIGHT");
     if( gdlYsize != "" && noQscreen) ySize=atoi(gdlYsize.c_str()); 
 #endif
-#endif
+
     e->AssureLongScalarKWIfPresent( "XSIZE", xSize);
     e->AssureLongScalarKWIfPresent( "YSIZE", ySize);
 
@@ -145,7 +137,7 @@ namespace lib {
 	  {
             DLong xSize, ySize;
             #ifdef HAVE_X
-                DeviceX::DefaultXYSize(&xSize, &ySize);
+                 actDevice->DefaultXYSize(&xSize, &ySize);
             #else
                 xSize = 640;
                 ySize = 512;
@@ -229,80 +221,6 @@ namespace lib {
       }
   }
 
-  // this code is issued from
-  // http://users.actcom.co.il/~choo/lupg/tutorials/xlib-programming/xlib-programming.html#display_info
-  // http://wwwcgi.rdg.ac.uk:8081/cgi-bin/cgiwrap/wsi14/poplog/pop11/ref/XHeightMMOfScreen 
-  BaseGDL* get_screen_size( EnvT* e)
-  {
-#ifndef HAVE_X
-    e->Throw("GDL was compiled without support for X-windows");
-    return NULL;
-#else
-    SizeT nParam=e->NParam(); 
     
-    if ( nParam > 1) e->Throw( "Incorrect number of arguments.");
-    char *TheDisplay=NULL;
-
-    if ( nParam == 1) {
-      DString GivenDisplay;
-      //Message( "Warning, we don't know exactly how to manage DISPLAY now.");
-      // this code was tested successfully on Ubuntu and OSX, through network
-      // and also with "strange" like 'localhost:0.0', '0:0' and "/tmp/launch-OfEkVY/:0" (OSX)
-      e->AssureStringScalarPar(0, GivenDisplay);
-      TheDisplay = new char [GivenDisplay.size()+1];
-      strcpy (TheDisplay, GivenDisplay.c_str());
-    }
-
-    /* open the connection to the display */
-    Display* display;
-    display = XOpenDisplay(TheDisplay);
-    if (display == NULL)
-      e->Throw("Cannot connect to X server");//, TheDisplay.c_str());
-    
-    int screen_num;
-    
-    /* these variables will store the size of the screen, in pixels.    */
-    int screen_width;
-    int screen_height;
-   
-    /* check the number of the default screen for our X server. */
-    screen_num = DefaultScreen(display);
-    
-    /* find the width of the default screen of our X server, in pixels. */
-    screen_width = DisplayWidth(display, screen_num);
-   
-    /* find the height of the default screen of our X server, in pixels. */
-    screen_height = DisplayHeight(display, screen_num);
-
-    //int debug=0;
-    //if (debug ==1) {
-    //  cout << "Nb pixels: " << screen_width << " x " << screen_height << endl;
-    //}
-
-    static int resolutionIx = e->KeywordIx( "RESOLUTION");
-    if( e->KeywordPresent( resolutionIx)) {
-      int screen_width_mm;
-      int screen_height_mm;
-      screen_width_mm = DisplayWidthMM(display, screen_num);
-      screen_height_mm = DisplayHeightMM(display, screen_num);
-      DDoubleGDL* resolution;
-      resolution = new DDoubleGDL(2, BaseGDL::NOZERO);
-      (*resolution)[0]=(screen_width_mm/10.)/screen_width;
-      (*resolution)[1]=(screen_height_mm/10.)/screen_height;
-      e->SetKW(0, resolution);
-      //if (debug ==1) {
-      //  cout << "screen (mm): " << screen_width_mm << " x " << screen_height_mm << endl;
-      //  cout << "pixels (mm): " << (*resolution)[0] << " x " << (*resolution)[1] << endl;
-      //}
-    }
-    
-    XCloseDisplay(display);
-    DDoubleGDL* res;
-    res = new DDoubleGDL(2, BaseGDL::NOZERO);
-    (*res)[0]=screen_width;
-    (*res)[1]=screen_height;
-    return res->Convert2(GDL_FLOAT, BaseGDL::CONVERT);
-#endif
-  }
 
 } // namespace
