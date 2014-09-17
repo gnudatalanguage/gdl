618a0bbad4bb3e643f4d70592e6e8d9c02ee0597
Alain Coulais <alain.coulais@obspm.fr>
2014-09-17 09:47:28 +0000

    here, we need to have a convertion into wxString to be able to compile ...

--- src/gdlwidget.hpp
+++ src/gdlwidget.hpp
@@ -557,7 +557,7 @@ public:
       switch( buttonType) {
 	case NORMAL: {
 	  wxButton* bb = static_cast<wxButton*>(wxWidget);
-	  bb->SetLabel(value);
+	  bb->SetLabel(wxString(value.c_str( ), wxConvUTF8));
 	  break;
 	}
       }
