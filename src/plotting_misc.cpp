5e14688c5feccfcd4b226a97a856434ffd21a951
Gilles Duvert <gilles.duvert@univ-grenoble-alpes.fr>
2014-11-24 23:47:56 +0000

    correct behaviour of tvlct

--- src/plotting_misc.cpp
+++ src/plotting_misc.cpp
@@ -65,172 +65,160 @@ namespace lib
         static unsigned bckTag=pStruct->Desc()->TagIndex("BACKGROUND");
       (*static_cast<DLongGDL*>(pStruct->GetTag(bckTag, 0)))[0]=0;
     }
-  }
+}
 
-  void tvlct(EnvT* e)
+void tvlct( EnvT* e ) {
+  int nParam = e->NParam( 1 ); //, "TVLCT");
+
+  static int getKW = e->KeywordIx( "GET" );
+  bool get = e->KeywordSet( getKW );
+  static int hlsKW = e->KeywordIx( "HLS" );
+  bool hls = e->KeywordSet( hlsKW );
+  static int hsvKW = e->KeywordIx( "HSV" );
+  bool hsv = e->KeywordSet( hsvKW );
+
+  DLong start = 0;
+  GDLCT* actCT = GraphicsDevice::GetCT( );
+  if ( nParam <= 2 ) // TVLCT,I[,Start]
   {
-    int nParam=e->NParam(1); //, "TVLCT");
+    if ( nParam == 2 )
+      e->AssureLongScalarPar( 1, start );
+    if ( start < 0 ) start = 0;
 
-    static int getKW=e->KeywordIx("GET");
-    bool get=e->KeywordSet(getKW);
-    static int hlsKW=e->KeywordIx("HLS");
-    bool hls=e->KeywordSet(hlsKW);
-    static int hsvKW=e->KeywordIx("HSV");
-    bool hsv=e->KeywordSet(hsvKW);
-
-    DLong start=0;
-    GDLCT* actCT=GraphicsDevice::GetCT();
-    if ( nParam<=2 ) // TVLCT,I[,Start]
+    if ( get ) // always RGB
     {
-      if ( nParam==2 )
-        e->AssureLongScalarPar(1, start);
-      if ( start<0 ) start=0;
+      BaseGDL*& p0 = e->GetParGlobal( 0 );
 
-      if ( get ) // always RGB
-      {
-        BaseGDL*& p0=e->GetParGlobal(0);
+      SizeT nCol = ctSize - start;
 
-        SizeT nCol=ctSize-start;
+      DByteGDL* rgb = new DByteGDL( dimension( nCol, 3 ), BaseGDL::NOZERO );
 
-        DByteGDL* rgb=new DByteGDL(dimension(nCol, 3), BaseGDL::NOZERO);
+      for ( SizeT i = start, ii = 0; i < ctSize; ++i, ++ii )
+        actCT->Get( i, (*rgb)[ ii], (*rgb)[ ii + nCol], (*rgb)[ ii + 2 * nCol] );
 
-        for ( SizeT i=start, ii=0; i<ctSize; ++i, ++ii )
-          actCT->Get(i, (*rgb)[ ii], (*rgb)[ ii+nCol], (*rgb)[ ii+2*nCol]);
+      GDLDelete( p0 );
+      p0 = rgb;
+    } else {
+      if ( hls || hsv ) {
+        DFloatGDL* rgb = e->GetParAs< DFloatGDL>(0);
+        if ( rgb->Dim( 1 ) != 3 )
+          e->Throw( "Array must have dimensions of (1, 3): " +
+        e->GetParString( 0 ) );
 
-        GDLDelete(p0);
-        p0=rgb;
-      }
-      else
-      {
-        if ( hls||hsv )
-        {
-          DFloatGDL* rgb=e->GetParAs< DFloatGDL>(0);
-          if ( rgb->Dim(1)!=3 )
-            e->Throw("Array must have dimensions of (1, 3): "+
-                     e->GetParString(0));
+        SizeT nCol = rgb->Dim( 0 );
+        SizeT setCol = nCol + start;
+        if ( setCol > ctSize ) setCol = ctSize;
 
-          SizeT nCol=rgb->Dim(0);
-          SizeT setCol=nCol+start;
-          if ( setCol>ctSize ) setCol=ctSize;
-
-          if ( hls )
-            for ( SizeT i=start, ii=0; i<setCol; ++i, ++ii )
-              actCT->SetHLS(i,
-                            (*rgb)[ ii],
-                            (*rgb)[ ii+nCol],
-                            (*rgb)[ ii+2*nCol]);
-          else
-            for ( SizeT i=start, ii=0; i<setCol; ++i, ++ii )
-              actCT->SetHSV(i,
-                            (*rgb)[ ii],
-                            (*rgb)[ ii+nCol],
-                            (*rgb)[ ii+2*nCol]);
-        }
+        if ( hls )
+          for ( SizeT i = start, ii = 0; i < setCol; ++i, ++ii )
+            actCT->SetHLS( i,
+          (*rgb)[ ii],
+          (*rgb)[ ii + nCol],
+          (*rgb)[ ii + 2 * nCol] );
         else
-        {
-          DByteGDL* rgb=e->GetParAs< DByteGDL>(0);
-          if ( rgb->Dim(1)!=3 )
-            e->Throw("Array must have dimensions of (1, 3): "+
-                     e->GetParString(0));
+          for ( SizeT i = start, ii = 0; i < setCol; ++i, ++ii )
+            actCT->SetHSV( i,
+          (*rgb)[ ii],
+          (*rgb)[ ii + nCol],
+          (*rgb)[ ii + 2 * nCol] );
+      } else {
+        DByteGDL* rgb = e->GetParAs< DByteGDL>(0);
+        if ( rgb->Dim( 1 ) != 3 )
+          e->Throw( "Array must have dimensions of (1, 3): " +
+        e->GetParString( 0 ) );
 
-          SizeT nCol=rgb->Dim(0);
-          SizeT setCol=nCol+start;
-          if ( setCol>ctSize ) setCol=ctSize;
+        SizeT nCol = rgb->Dim( 0 );
+        SizeT setCol = nCol + start;
+        if ( setCol > ctSize ) setCol = ctSize;
 
-          for ( SizeT i=start, ii=0; i<setCol; ++i, ++ii )
-            actCT->Set(i,
-                       (*rgb)[ ii],
-                       (*rgb)[ ii+nCol],
-                       (*rgb)[ ii+2*nCol]);
-        }
+        for ( SizeT i = start, ii = 0; i < setCol; ++i, ++ii )
+          actCT->Set( i,
+        (*rgb)[ ii],
+        (*rgb)[ ii + nCol],
+        (*rgb)[ ii + 2 * nCol] );
       }
     }
-    else // TVLCT,I1,I2,I3[,Start]
+  } else // TVLCT,I1,I2,I3[,Start]
+  {
+    if ( nParam == 4 )
+      e->AssureLongScalarPar( 3, start );
+    if ( start < 0 ) start = 0;
+
+    if ( get ) // always RGB
     {
-      if ( nParam==4 )
-        e->AssureLongScalarPar(3, start);
-      if ( start<0 ) start=0;
+      BaseGDL*& p0 = e->GetParGlobal( 0 );
+      BaseGDL*& p1 = e->GetParGlobal( 1 );
+      BaseGDL*& p2 = e->GetParGlobal( 2 );
 
-      if ( get ) // always RGB
-      {
-        BaseGDL*& p0=e->GetParGlobal(0);
-        BaseGDL*& p1=e->GetParGlobal(1);
-        BaseGDL*& p2=e->GetParGlobal(2);
+      SizeT nCol = ctSize - start;
 
-        SizeT nCol=ctSize-start;
+      DByteGDL* r = new DByteGDL( dimension( nCol ), BaseGDL::NOZERO );
+      DByteGDL* g = new DByteGDL( dimension( nCol ), BaseGDL::NOZERO );
+      DByteGDL* b = new DByteGDL( dimension( nCol ), BaseGDL::NOZERO );
 
-        DByteGDL* r=new DByteGDL(dimension(nCol), BaseGDL::NOZERO);
-        DByteGDL* g=new DByteGDL(dimension(nCol), BaseGDL::NOZERO);
-        DByteGDL* b=new DByteGDL(dimension(nCol), BaseGDL::NOZERO);
+      for ( SizeT i = start, ii = 0; i < ctSize; ++i, ++ii )
+        actCT->Get( i, (*r)[ ii], (*g)[ ii], (*b)[ ii] );
 
-        for ( SizeT i=start, ii=0; i<ctSize; ++i, ++ii )
-          actCT->Get(i, (*r)[ ii], (*g)[ ii], (*b)[ ii]);
+      GDLDelete( p0 );
+      p0 = r;
+      GDLDelete( p1 );
+      p1 = g;
+      GDLDelete( p2 );
+      p2 = b;
+    } else {
+      if ( hls || hsv ) {
+        DFloatGDL* r = e->GetParAs< DFloatGDL>(0);
+        DFloatGDL* g = e->GetParAs< DFloatGDL>(1);
+        DFloatGDL* b = e->GetParAs< DFloatGDL>(2);
+        SizeT rCol = r->N_Elements( );
+        SizeT gCol = g->N_Elements( );
+        SizeT bCol = b->N_Elements( );
+        SizeT nCol = rCol;
+        if ( gCol < nCol ) nCol = gCol;
+        if ( bCol < nCol ) nCol = bCol;
 
-        GDLDelete(p0);
-        p0=r;
-        GDLDelete(p1);
-        p1=g;
-        GDLDelete(p2);
-        p2=b;
-      }
-      else
-      {
-        if ( hls||hsv )
-        {
-          DFloatGDL* r=e->GetParAs< DFloatGDL>(0);
-          DFloatGDL* g=e->GetParAs< DFloatGDL>(1);
-          DFloatGDL* b=e->GetParAs< DFloatGDL>(2);
-          SizeT rCol=r->N_Elements();
-          SizeT gCol=g->N_Elements();
-          SizeT bCol=b->N_Elements();
-          SizeT nCol=rCol;
-          if ( gCol<nCol ) nCol=gCol;
-          if ( bCol<nCol ) nCol=bCol;
+        SizeT setCol = nCol + start;
+        if ( setCol > ctSize ) setCol = ctSize;
 
-          SizeT setCol=nCol+start;
-          if ( setCol>ctSize ) setCol=ctSize;
-
-          if ( hls )
-            for ( SizeT i=start, ii=0; i<setCol; ++i, ++ii )
-              actCT->SetHLS(i, (*r)[ ii], (*g)[ ii], (*b)[ ii]);
-          else
-            for ( SizeT i=start, ii=0; i<setCol; ++i, ++ii )
-              actCT->SetHSV(i, (*r)[ ii], (*g)[ ii], (*b)[ ii]);
-        }
+        if ( hls )
+          for ( SizeT i = start, ii = 0; i < setCol; ++i, ++ii )
+            actCT->SetHLS( i, (*r)[ ii], (*g)[ ii], (*b)[ ii] );
         else
-        {
-          DByteGDL* r=e->GetParAs< DByteGDL>(0);
-          DByteGDL* g=e->GetParAs< DByteGDL>(1);
-          DByteGDL* b=e->GetParAs< DByteGDL>(2);
-          SizeT rCol=r->N_Elements();
-          SizeT gCol=g->N_Elements();
-          SizeT bCol=b->N_Elements();
-          SizeT nCol=rCol;
-          if ( gCol<nCol ) nCol=gCol;
-          if ( bCol<nCol ) nCol=bCol;
+          for ( SizeT i = start, ii = 0; i < setCol; ++i, ++ii )
+            actCT->SetHSV( i, (*r)[ ii], (*g)[ ii], (*b)[ ii] );
+      } else {
+        DByteGDL* r = e->GetParAs< DByteGDL>(0);
+        DByteGDL* g = e->GetParAs< DByteGDL>(1);
+        DByteGDL* b = e->GetParAs< DByteGDL>(2);
+        SizeT rCol = r->N_Elements( );
+        SizeT gCol = g->N_Elements( );
+        SizeT bCol = b->N_Elements( );
+        SizeT nCol = rCol;
+        if ( gCol < nCol ) nCol = gCol;
+        if ( bCol < nCol ) nCol = bCol;
 
-          SizeT setCol=nCol+start;
-          if ( setCol>ctSize ) setCol=ctSize;
+        SizeT setCol = nCol + start;
+        if ( setCol > ctSize ) setCol = ctSize;
 
-          for ( SizeT i=start, ii=0; i<setCol; ++i, ++ii )
-            actCT->Set(i, (*r)[ ii], (*g)[ ii], (*b)[ ii]);
-        }
-      }
-    }
-
-    // AC, 07/02/2012, please report any unexpected side effect (see test_tvlct.pro)
-    if ( !get )
-    {
-      GDLGStream* actStream=GraphicsDevice::GetDevice()->GetStream(false);
-      if ( actStream!=NULL )
-      {
-        PLINT red[ctSize], green[ctSize], blue[ctSize];
-        actCT->Get(red, green, blue);
-        actStream->scmap0(red, green, blue, ctSize);
+        for ( SizeT i = start, ii = 0; i < setCol; ++i, ++ii )
+          actCT->Set( i, (*r)[ ii], (*g)[ ii], (*b)[ ii] );
       }
     }
   }
-  
+
+  // AC, 07/02/2012, please report any unexpected side effect (see test_tvlct.pro)
+  if ( !get ) {
+    PLINT red[ctSize], green[ctSize], blue[ctSize];
+    actCT->Get( red, green, blue );
+    GraphicsDevice* actDevice = GraphicsDevice::GetDevice( );
+    int nbActiveStreams = actDevice->MaxWin( ); //new colormap must be given to *all* streams.
+    for ( int i = 0; i < nbActiveStreams; ++i ) {
+      GDLGStream* actStream = actDevice->GetStreamAt( i );
+      if ( actStream != NULL ) actStream->scmap0( red, green, blue, ctSize );
+    }
+  }
+}
+
   BaseGDL* get_screen_size( EnvT* e)
   {
     //GD: DO NOT NEVER EVER TOUCH THIS WITHOUT TRIPLE THINKING.
