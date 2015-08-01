b48dfa5f6f70632d5cd3f1b2307843eb36b53628
Alain Coulais <alain.coulais@obspm.fr>
2015-08-01 06:38:51 +0000

    B argument to LA_LEAST_SQUARES can now be an array. Other details fixed.

--- src/least_squares.cpp
+++ src/least_squares.cpp
@@ -1,9 +1,9 @@
 /***************************************************************************
-                          least_squares.cpp  - GDL library function
+                          la_least_squares.cpp  -  GDL library function
                              -------------------
     begin                : Jul 2 2015
     copyright            : (C) 2015 by NATCHKEBIA Ilia
-    email                : alaingdl@users.sourceforge.net
+    email contact        : alaingdl@users.sourceforge.net
 ***************************************************************************/
 
 /***************************************************************************
@@ -30,8 +30,6 @@
 #include "basic_fun.hpp"
 #include "dinterpreter.hpp"
 
-//#include "gsl_fun.hpp"
-
 #if defined(USE_EIGEN)
 #include <Eigen/LU>
 #include <Eigen/Eigenvalues>
@@ -56,10 +54,12 @@ namespace lib {
    ********************la_least_squares_Solution***************
    ************************************************************/
   BaseGDL* la_least_squares_fun( EnvT* e) {
+    
 
+    
     static int method = 3;  //default Method is 3 that give identic result as idl
     static int isDouble = 0;   //default double is 0
-    if (e->KeywordSet("METHOD")) e->AssureLongScalarKWIfPresent( e->KeywordIx("METHOD"), method);
+    e->AssureLongScalarKWIfPresent( e->KeywordIx("METHOD"), method);
     if (e->KeywordSet("DOUBLE")) isDouble = e->KeywordIx( "DOUBLE");
     
     SizeT nParam=e->NParam(2);
@@ -67,47 +67,59 @@ namespace lib {
     BaseGDL* p1 = e->GetParDefined( 1);
 
 
-    /***************Checking_if_arguments_are_OK*********************/
+    /***************Checking_if_arguments_are_OK***********/
 
     if (p0->N_Elements()==0)
       e->Throw( "Variable A is undefined: " + e->GetParString(0));
     if (p1->N_Elements()==0)
       e->Throw( "Variable B is undefined: " + e->GetParString(1));
-    if (p0->Rank() < 2){
-      e->Throw( "Argument A must be a multi dimensional matrix:" + e->GetParString(0));
+    if (p0->Rank() != 2){
+      e->Throw( "Argument A must be a 2 dimensional matrix:" + e->GetParString(0));
     }
-    if (p1->Rank() != 1) {
-      e->Throw( "Argument B must be a vector:" + e->GetParString(1));
+    if (p1->Rank() > 2) {
+      e->Throw( "Argument B must be a vector or 2 dimensional array:" + e->GetParString(1));
     }
-    if(p1->N_Elements()!=p0->Dim(1)) {
-      e->Throw("Number of elements in B does not match number of rows in A");
+    if(p1->Rank() == 1) {
+      if(p1->N_Elements()!=p0->Dim(1))
+	e->Throw("Number of elements in B does not match number of rows in A");
+    }else if (p1->Dim(1)!=p0->Dim(1)){
+      e->Throw("Number of rows in B does not match number of rows in A");
     }
     if(0 > method || method > 3) {
       e->Throw("Method must be 0,1,2 or 3");
     }
     
-    long NbCol,NbRow;
+    long NbCol1,NbRow1,NbCol2,NbRow2;
+	
 
-    /*****************Variables******************************/
 
+    /**********************Variables************************/
     DDoubleGDL* p0D = static_cast<DDoubleGDL*>
       (p0->Convert2( GDL_DOUBLE, BaseGDL::COPY));
     DDoubleGDL* p2D = static_cast<DDoubleGDL*>
       (p1->Convert2( GDL_DOUBLE, BaseGDL::COPY));
-    NbCol=p0->Dim(1);
-    NbRow=p0->Dim(0);
-    Map<Matrix<double,Dynamic,Dynamic,RowMajor> > m0(&(*p0D)[0], NbCol,NbRow);
-    Map<VectorXd> m2(&(*p2D)[0], NbCol);
-
-    LLT<MatrixXd,Upper>solver;
-    VectorXd tmp_res;
-    /******************************Methods****************************/	
-    if (method == 0)
+    NbCol1=p0->Dim(1);
+    NbRow1=p0->Dim(0);
+    NbCol2=p1->Dim(1);
+    NbRow2=p1->Dim(0);
+    Map<Matrix<double,Dynamic,Dynamic,RowMajor> > m0(&(*p0D)[0], NbCol1,NbRow1);
+    Map<Matrix<double,Dynamic,Dynamic,RowMajor> > m2(&(*p2D)[0], NbCol2,NbRow2);
+    LLT<MatrixXf>solver;
+    Map<VectorXd> m3(&(*p2D)[0], NbRow2); //for one B when it's vector
+    MatrixXd tmp_res;
+    /**************Methods******************/	
+    if (method == 0 && p1->Dim(1))
       tmp_res = m0.colPivHouseholderQr().solve(m2) ;
-    else if (method == 1)
+    else if (method == 0 && !p1->Dim(1))
+      tmp_res = m0.colPivHouseholderQr().solve(m3) ;
+    else if (method == 1 && p1->Dim(1))
       tmp_res = (m0.transpose() * m0).ldlt().solve(m0.transpose() * m2) ;
-    else if (method >= 2) //method 2 and method 3 is the same
+    else if (method == 1 && !p1->Dim(1))
+      tmp_res = (m0.transpose() * m0).ldlt().solve(m0.transpose() * m3) ;
+    else if (method >= 2 && p1->Dim(1)) //method 2 and method 3 is the same
       tmp_res = m0.jacobiSvd(ComputeThinU | ComputeThinV).solve(m2) ;
+    else if (method >= 2 && !p1->Dim(1)) 
+      tmp_res = m0.jacobiSvd(ComputeThinU | ComputeThinV).solve(m3) ;
 
     if(solver.info()==NumericalIssue) 
       {
@@ -119,18 +131,35 @@ namespace lib {
 	e->Throw( "Decomposition has failed: " + e->GetParString(0));
 	return 0;
       }
-    /***********************Return Values*****************************/
+    /***********************Return Values******************/
+    if(p1->Dim(1)) {
+      SizeT dims[3];
+      dims[1] = NbRow1;
+      dims[0] = NbRow2;
+      dimension dim( dims, (SizeT) 2 );
 
-    if(( p0->Type() == GDL_DOUBLE) || e->KeywordSet("DOUBLE")) {
-      DDoubleGDL* resD =new DDoubleGDL(NbRow, BaseGDL::NOZERO);
-      Map<VectorXd>(&(*resD)[0], NbRow) = tmp_res.cast<double>();
-      return resD;
-    } else {
-      DFloatGDL* resF = new DFloatGDL(NbRow, BaseGDL::NOZERO);
-      Map<VectorXf>(&(*resF)[0], NbRow) = tmp_res.cast<float>();
-      return resF;
+      if(( p0->Type() == GDL_DOUBLE) || e->KeywordSet("DOUBLE")) {
+	DDoubleGDL* resD =new DDoubleGDL(dim, BaseGDL::NOZERO);
+	Map<MatrixXd>(&(*resD)[0], NbRow2,NbRow1) = tmp_res.transpose().cast<double>();
+	return resD;
+      } else {
+			
+	DFloatGDL* resF = new DFloatGDL(dim, BaseGDL::NOZERO);
+	Map<MatrixXf>(&(*resF)[0], NbRow2,NbRow1) = tmp_res.transpose().cast<float>();
+	return resF;
+      }
+    } else { //when B is a vector
+      if(( p0->Type() == GDL_DOUBLE) || e->KeywordSet("DOUBLE")) {
+	DDoubleGDL* resD =new DDoubleGDL(NbRow1, BaseGDL::NOZERO);
+	Map<VectorXd>(&(*resD)[0], NbRow1) = tmp_res.cast<double>();
+	return resD;
+      } else {
+			
+	DFloatGDL* resF = new DFloatGDL(NbRow1, BaseGDL::NOZERO);
+	Map<VectorXf>(&(*resF)[0], NbRow1) = tmp_res.cast<float>();
+	return resF;
+      }
     }
-
   }
 
 #endif
