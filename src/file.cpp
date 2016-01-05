a3eb8267706feb60f868d7d77f00eea78a75d11a
Gilles Duvert <gilles.duvert@univ-grenoble-alpes.fr>
2016-01-05 23:05:48 +0000

    applied patch #98

--- src/file.cpp
+++ src/file.cpp
@@ -241,11 +241,11 @@ extern "C"
 */
 #endif
 #define NTEST_SEARCH 7
-
-#ifdef __CYGWIN__
+#if defined(__CYGWIN__) || defined(__FreeBSD__)
 #define stat64 stat
 #define lstat64 lstat
 // for religious reasons, CYGWIN doesn't do lstat64
+// FreeBSD doesn't do lstat64 because there is no need for it
 #endif
 
 namespace lib {
