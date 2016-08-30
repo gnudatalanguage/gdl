bbc6719e36ba514ca642e348976104673c3cf468
Alain Coulais <alain.coulais@obspm.fr>
2016-08-30 14:01:15 +0000

    better version for FindPlplot.cmake ! When PLPLOTDIR= is given, it must be enforce, no way to look outside !

--- CMakeModules/FindPlplot.cmake
+++ CMakeModules/FindPlplot.cmake
@@ -9,31 +9,57 @@
 #
 #test of rare case where old and new libraries are still present: terrible trouble!
 
-# the following code is justify to manage plplot
-# on OSX 10.11 where the current versions of PLplot
+# the following code is justify to manage PLplot
+# on OSX 10.11 where the current versions of PLplot (30 August 2016)
 # packaged in Brew and Fink may be not OK with the CVS version of GDL
-# Then, when PLPLOTDIR is provide, it must be strickly enforce,
+# Then, when PLPLOTDIR is provide, it must be *strickly enforce*,
 # not looking at the lib. provided by Brew or Fink
 # 
+#message(" ${PLPLOTDIR}")
+#  find_library(NEW_PLPLOT_LIBRARY NAMES plplot PATHS ${PLPLOTDIR} NO_DEFAULT_PATH)
+#  find_library(OLD_PLPLOT_LIBRARY NAMES plplotd PATHS ${PLPLOTDIR} NO_DEFAULT_PATH)
+#message("new ${NEW_PLPLOT_LIBRARY}")
+#message("old ${OLD_PLPLOT_LIBRARY}")
+
 if(PLPLOTDIR)
-  find_library(NEW_PLPLOT_LIBRARY NAMES plplot NO_DEFAULT_PATH)
-  find_library(OLD_PLPLOT_LIBRARY NAMES plplotd NO_DEFAULT_PATH)
-else()
+  message("Strickly enforce PLPLOTDIR=${PLPLOTDIR}")
+  find_library(NEW_PLPLOT_LIBRARY NAMES plplot PATHS ${PLPLOTDIR} NO_DEFAULT_PATH)
+  if(NEW_PLPLOT_LIBRARY)
+      find_library(PLPLOT_LIBRARY NAMES plplot PATHS ${PLPLOTDIR} NO_DEFAULT_PATH)
+      find_library(PLPLOTCXX_LIBRARY NAMES plplotcxx PATHS ${PLPLOTDIR} NO_DEFAULT_PATH)
+  endif(NEW_PLPLOT_LIBRARY)
+  #
+  find_library(OLD_PLPLOT_LIBRARY NAMES plplotd PATHS ${PLPLOTDIR} NO_DEFAULT_PATH)
+  if(OLD_PLPLOT_LIBRARY)
+      find_library(PLPLOT_LIBRARY NAMES plplotd PATHS ${PLPLOTDIR} NO_DEFAULT_PATH)
+      find_library(PLPLOTCXX_LIBRARY NAMES plplotcxxd PATHS ${PLPLOTDIR} NO_DEFAULT_PATH)
+  endif(OLD_PLPLOT_LIBRARY)
+else(PLPLOTDIR)
+  message("Use fuzzy detection for PLplot lib. (e.g. in /usr/lib)")
   find_library(NEW_PLPLOT_LIBRARY NAMES plplot)
+  if(NEW_PLPLOT_LIBRARY)
+      find_library(PLPLOT_LIBRARY NAMES plplot)
+      find_library(PLPLOTCXX_LIBRARY NAMES plplotcxx)
+  endif(NEW_PLPLOT_LIBRARY)
+  #
   find_library(OLD_PLPLOT_LIBRARY NAMES plplotd)
-endif()
+  if(OLD_PLPLOT_LIBRARY)
+      find_library(PLPLOT_LIBRARY NAMES plplotd)
+      find_library(PLPLOTCXX_LIBRARY NAMES plplotcxxd)
+  endif(OLD_PLPLOT_LIBRARY)
+endif(PLPLOTDIR)
+
+#message("new ${NEW_PLPLOT_LIBRARY}")
+#message("old ${OLD_PLPLOT_LIBRARY}")
+
 
 if (NEW_PLPLOT_LIBRARY AND OLD_PLPLOT_LIBRARY) 
-    message(WARNING "Two incompatible plplot libraries are installed, probable trouble at compile time ahead.")
+    message(FATAL "Two incompatible plplot libraries are installed, probable trouble at compile time ahead.")
 endif (NEW_PLPLOT_LIBRARY AND OLD_PLPLOT_LIBRARY)
 
-# the logic here is to use first "plplot" version, if not then "plplotd" one
-find_library(PLPLOT_LIBRARY NAMES plplot)
-find_library(PLPLOTCXX_LIBRARY NAMES plplotcxx)
-if (NOT PLPLOT_LIBRARY)
-    find_library(PLPLOT_LIBRARY NAMES plplotd)
-    find_library(PLPLOTCXX_LIBRARY NAMES plplotcxxd)
-endif(NOT PLPLOT_LIBRARY)
+#      find_library(PLPLOT_LIBRARY NAMES plplot NO_DEFAULT_PATH)
+#      find_library(PLPLOTCXX_LIBRARY NAMES plplotcxx NO_DEFAULT_PATH)
+#message(" ${PLPLOT_LIBRARY}")
 
 # normal situation : take care that PLPLOT_LIBRARIES and PLPLOT_INCLUDE_DIR prefix
 # must be the same !!
