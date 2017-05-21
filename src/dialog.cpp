351a8d589aa589f716f82447755bdffff03f4291
Gilles Duvert <gilles.duvert@univ-grenoble-alpes.fr>
2017-05-21 14:24:02 +0000

    should solve bug 722.

--- src/dialog.cpp
+++ src/dialog.cpp
@@ -18,8 +18,9 @@
 #include "includefirst.hpp"
 
 #ifdef HAVE_LIBWXWIDGETS
-#include <wx/filedlg.h>
-#include <wx/msgdlg.h>
+//following includes are 1) not necessary and 2) create a problem with gcc6
+//#include <wx/filedlg.h>
+//#include <wx/msgdlg.h>
 #endif
 
 #include "envt.hpp"
