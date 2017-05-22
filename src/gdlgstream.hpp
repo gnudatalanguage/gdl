52ae196a8c08dc16be3c0c025fa2a33261ff9748
Gilles Duvert <gilles.duvert@univ-grenoble-alpes.fr>
2017-05-22 12:20:23 +0000

    applied patch #104

--- src/gdlgstream.hpp
+++ src/gdlgstream.hpp
@@ -199,7 +199,7 @@ public:
     const char **devnames = NULL;
 #endif
 
-    static vector<std::string> devNames;
+    static std::vector<std::string> devNames;
 
     // do only once
     if( devNames.empty())
--- src/gdlwidget.cpp
+++ src/gdlwidget.cpp
@@ -2455,7 +2455,7 @@ BaseGDL* GDLWidgetTable::GetTableValuesAsStruct(DLongGDL* selection)
   } 
   else { //use the wxWidget selection or the passed selection, mode-dependent:
     if (disjointSelection) { //pairs lists
-      vector<wxPoint> list;
+      std::vector<wxPoint> list;
       if (selection->Rank()==0) { //use current wxWidgets selection. Result is a STRUCT
         list=grid->GetSelectedDisjointCellsList();
       } else {                   //make equivalent vector.
