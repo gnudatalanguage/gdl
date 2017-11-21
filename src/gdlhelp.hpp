569ce63a0ccf0d1043b9475c09bd8ecc450dab48
Gilles Duvert <gilles.duvert@univ-grenoble-alpes.fr>
2017-11-21 21:28:21 +0000

    separate help-related functions from basic_pro.cpp, patch "stage0" by G. Jung, see bug #725

---
+++ src/gdlhelp.hpp
@@ -0,0 +1,44 @@
+/***************************************************************************
+                          gdlhelp.hpp  -  basic GDL library procedures
+                             -------------------
+    begin                : May 01 2015
+    copyright            : (C) 2002 by Marc Schellens
+    email                : m_schellens@users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef GDLHELP_HPP_
+#define GDLHELP_HPP_
+
+namespace lib {
+
+  
+  // help_item is used by EnvT::HeapGC
+  void help_item( std::ostream& os,
+		  BaseGDL* par, DString parString, bool doIndentation);
+  void help_help( EnvT* e); // This may be useful and easy to call
+						// directly from interpreter
+  void help_struct( std::ostream& os,  BaseGDL* par, int indent , bool debug );
+  
+   // the following by Alain Coulais
+  // (alaingdl @@ users.sourceforge.net)
+  BaseGDL* recall_commands(EnvT* e);
+
+//  BaseGDL* routine_filepath( EnvT* e); this is in basic_fun
+
+  void help_pro( EnvT* e);
+
+//  void delvar_pro( EnvT* e);
+//  void findvar_pro( EnvT* e);
+
+  } // namespace
+
+#endif
