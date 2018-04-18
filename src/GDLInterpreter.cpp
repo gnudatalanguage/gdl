2960b82689ef4dd57961341d04b6bcbce9fa67fc
gildas <gildas@localhost>
2018-04-18 14:36:46 +0200

    changes in PR #112 were done to GDLInterpreter and not to gdlc.i.g This went unnoticed, and this is bad, as all GDL*** files are recreated from antlr .g files. We have to add a script in Travis checking that antlr-compiling .g files reproduce the exiting GDL* files.

--- src/GDLInterpreter.cpp
+++ src/GDLInterpreter.cpp
@@ -1,4 +1,4 @@
-/* $ANTLR 2.7.7 (20130428): "gdlc.i.g" -> "GDLInterpreter.cpp"$ */
+/* $ANTLR 2.7.7 (2006-11-01): "gdlc.i.g" -> "GDLInterpreter.cpp"$ */
 
     // gets inserted before the antlr generated includes in the cpp file
 #include "includefirst.hpp"
