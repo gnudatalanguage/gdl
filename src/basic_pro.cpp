3ba4c43b2533d7d144d40d9e60092aa90f8e1817
Greg Jung <gvjung@gmail.com>
2018-04-19 00:13:12 -0700

    Refurb of launch_cmd (called from spawn_pro fror Windows)
      This modification takes the (c char) -> (w_char) manipulations into the launch_cmd
    routine, so that the calling routine, spawn_pro for WIN32 case, only sees UTF8
    strings. MultiByteToWideChar only appears prior to its need in launch_cmd, which invokes
    CreateProcessW. This only affects Windows operation.
      Also revised to use the current directory for the spawned process.
    
    Greg Jung (aka maynardGK)
    gvjung@gmail.com

--- src/basic_pro.cpp
+++ src/basic_pro.cpp
@@ -58,7 +58,7 @@
 namespace lib {
 
   using namespace std;
-
+	DString GetCWD(); // From file.cpp
   // control !CPU settings
 
   void cpu(EnvT* e) {
@@ -1594,9 +1594,10 @@ namespace lib {
     }
     *pos = ptr;
   }
-
-  DWORD launch_cmd(BOOL hide, BOOL nowait, LPWSTR cmd, LPWSTR title = NULL, DWORD *pid = NULL,
-    vector<DString> *ds_outs = NULL, vector<DString> *ds_errs = NULL) {
+static DWORD launch_cmd(BOOL hide, BOOL nowait,
+                     const char * cmd, const char * title = NULL, DWORD *pid = NULL,
+		     vector<DString> *ds_outs = NULL, vector<DString> *ds_errs = NULL)
+    {
     DWORD status;
     CHAR outbuf[BUFSIZE];
     CHAR errbuf[BUFSIZE];
@@ -1604,6 +1605,15 @@ namespace lib {
     STARTUPINFOW si = {0,};
     PROCESS_INFORMATION pi = {0,};
 
+      WCHAR w_cmd[1000];
+      MultiByteToWideChar(CP_UTF8, 0, cmd, -1, w_cmd, 1000);
+      WCHAR w_title[100];
+      if( title != NULL)
+              MultiByteToWideChar(CP_UTF8, 0, title, -1, w_title, 100);
+      WCHAR w_cwd[MAX_PATH];
+	DString cwd = lib::GetCWD();
+             MultiByteToWideChar(CP_UTF8, 0, cwd.c_str(), -1, w_cwd, MAX_PATH);
+	
     SECURITY_ATTRIBUTES saAttr;
 
     saAttr.nLength = sizeof (SECURITY_ATTRIBUTES);
@@ -1617,9 +1627,9 @@ namespace lib {
 
     si.cb = sizeof (si);
     if (title == NULL)
-      si.lpTitle = cmd;
+        si.lpTitle = (wchar_t *) L"GDL spawned process";
     else
-      si.lpTitle = title;
+        si.lpTitle = w_title;
     int debug = 0;
 
     if (hide) {
@@ -1641,7 +1651,9 @@ namespace lib {
       }
       si.dwFlags |= STARTF_USESTDHANDLES;
       if (debug) std::printf(" CreateProcess: ");
-      CreateProcessW(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+        CreateProcessW(NULL, w_cmd, NULL, NULL, TRUE,
+						 0, NULL, w_cwd,	// start from wherever we happen to be 
+								&si, &pi);
       if (pid != NULL) *pid = pi.dwProcessId;
       DWORD progress;
 
@@ -1683,8 +1695,13 @@ namespace lib {
       } else
         std::printf(" error from CreateProcess: progress = 0x%x \n", progress);
 
-    } else {
-      CreateProcessW(NULL, cmd, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+      }
+      else
+	{
+          CreateProcessW(NULL, w_cmd, NULL, NULL, FALSE,
+                        CREATE_NEW_CONSOLE, 
+                        NULL, w_cwd,	// start from wherever we happen to be 
+								&si, &pi);
       if (pid != NULL) *pid = pi.dwProcessId;
       if (!nowait) WaitForSingleObject(pi.hProcess, INFINITE);
       GetExitCodeProcess(pi.hProcess, &status);
@@ -1739,8 +1756,10 @@ namespace lib {
        */
     }
 
-    if (nParam == 0) {
-      DWORD status = launch_cmd(hideKeyword, nowaitKeyword, (LPWSTR) L"cmd", (LPWSTR) L"Command Prompt");
+      if (nParam == 0)
+	{
+          DWORD status = launch_cmd(hideKeyword, nowaitKeyword,
+                              "cmd", " (GDL-Spawned) Command Prompt");
       if (countKeyword)
         e->SetKW(countIx, new DLongGDL(0));
       if (exit_statusKeyword)
@@ -1761,20 +1780,17 @@ namespace lib {
       ds_cmd = cmd;
     else
       ds_cmd = "cmd /c " + cmd;
-
-    WCHAR w_cmd[255];
-    MultiByteToWideChar(CP_ACP, 0, ds_cmd.c_str(), ds_cmd.length(), w_cmd, 255);
-
     vector<DString> ds_outs;
     vector<DString> ds_errs;
     int status;
     DWORD pid;
     if (nParam == 1)
-      status = launch_cmd(hideKeyword, nowaitKeyword, w_cmd, NULL, &pid);
+        status = launch_cmd(hideKeyword, nowaitKeyword, ds_cmd.c_str(), NULL, &pid);
     else if (nParam == 2) {
-      status = launch_cmd(hideKeyword, nowaitKeyword, w_cmd, NULL, &pid, &ds_outs);
-    } else if (nParam == 3) {
-      status = launch_cmd(hideKeyword, nowaitKeyword, w_cmd, NULL, &pid, &ds_outs, &ds_errs);
+        status = launch_cmd(hideKeyword, nowaitKeyword, ds_cmd.c_str(), NULL, &pid, &ds_outs);
+      }
+      else if (nParam == 3) {
+        status = launch_cmd(hideKeyword, nowaitKeyword, ds_cmd.c_str(), NULL, &pid, &ds_outs, &ds_errs);
     }
 
     if (pidKeyword)
