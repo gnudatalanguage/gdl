d1fcdeabdf127aa3f12e75468cf7705c217882ec
Remi A. Solås <remi.solaas@edinsights.no>
2018-06-20 15:23:29 +0200

    Improved CMake rules for TIFF and GeoTIFF

--- CMakeModules/FindGeoTIFF.cmake
+++ CMakeModules/FindGeoTIFF.cmake
@@ -5,7 +5,7 @@
 find_path(GEOTIFF_INCLUDE_DIR geotiff.h PATH_SUFFIXES geotiff)
 find_library(GEOTIFF_LIBRARY NAMES geotiff libgeotiff)
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GEOTIFF DEFAULT_MSG GEOTIFF_LIBRARY GEOTIFF_INCLUDE_DIR)
+find_package_handle_standard_args(GeoTIFF DEFAULT_MSG GEOTIFF_INCLUDE_DIR GEOTIFF_LIBRARY)
 
 if(GEOTIFF_FOUND)
     set(GEOTIFF_LIBRARIES ${GEOTIFF_LIBRARY})
@@ -15,4 +15,7 @@ endif(GEOTIFF_FOUND)
 mark_as_advanced(
 GEOTIFF_INCLUDE_DIR
 GEOTIFF_LIBRARY
+GEOTIFF_LIBRARIES
+GEOTIFF_INCLUDE_DIRS
 )
+
