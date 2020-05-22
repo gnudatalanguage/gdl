584e16e902295c54c56f3c0f3c8bb9b78a33d3ef
GillesDuvert <gilles.duvert@free.fr>
2020-05-22 10:36:35 +0200

    Although heaps for Pointers and Vectors are separate in code (they are different objects), they share the same incremental index. This permits to mimic the (apparently) single heap list of IDL. Thus, heap indexes in IDL and GDL (see help,/heap) should be *normally* the same. This is more or less the same as for the numbering of widgets.
    Starting from now, a difference in heap indexes after some "exercising" of GDL on a complicated program could well reveal an hidden issue.

--- src/GDLInterpreter.hpp
+++ src/GDLInterpreter.hpp
@@ -3,7 +3,7 @@
 
 #include <antlr/config.hpp>
 #include "GDLInterpreterTokenTypes.hpp"
-/* $ANTLR 2.7.7 (20190904): "gdlc.i.g" -> "GDLInterpreter.hpp"$ */
+/* $ANTLR 2.7.7 (2006-11-01): "gdlc.i.g" -> "GDLInterpreter.hpp"$ */
 #include <antlr/TreeParser.hpp>
 
 
@@ -230,10 +230,10 @@ public:
     // the New... functions 'own' their BaseGDL*
     SizeT NewObjHeap( SizeT n=1, DStructGDL* var=NULL)
     {
-        SizeT tmpIx=objHeapIx;
+        SizeT tmpIx=heapIx;
         for( SizeT i=0; i<n; i++)
         objHeap.insert( objHeap.end(),
-            std::pair<SizeT, RefDStructGDL>( objHeapIx++, (DStructGDL*)var));
+            std::pair<SizeT, RefDStructGDL>( heapIx++, (DStructGDL*)var));
         return tmpIx;
     }
     SizeT NewHeap( SizeT n=1, BaseGDL* var=NULL)
@@ -248,7 +248,7 @@ public:
     {
         BaseGDL* del = (*it).second.get();
         objHeap.erase( id); 
-        if (!NullGDL::IsNULLorNullGDL(del)) delete del; //avoid destroying !NULL
+        if (!NullGDL::IsNULLorNullGDL(del)) delete del;
     }
     static void FreeObjHeap( DObj id)
     {
@@ -718,7 +718,6 @@ std::cout << add << " + <ObjHeapVar" << id << ">" << std::endl;
         }
 // The counters are reset for easier human readability.
        heapIx = 1;
-       objHeapIx = 1;
     }
 
     // name of data
