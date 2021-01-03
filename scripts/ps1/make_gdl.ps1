f6e1d6a13395a6064a374fd5fb4c98027e953a8a
Jeongbin Park <pjb7687@gmail.com>
2021-01-03 04:25:17 +0100

    Update make_gdl.ps1

--- scripts/ps1/make_gdl.ps1
+++ scripts/ps1/make_gdl.ps1
@@ -2,5 +2,5 @@
 # This is needed because any write to stderr, which is used by gcc and cmake,
 # will invoke a powershell error.
 
-C:\msys64\usr\bin\bash.exe -lc "cd /c/projects/gdl &&. ./scripts/appveyor_gdl.msys"
+C:\msys64\usr\bin\bash.exe -lc "cd /c/projects/gdl &&. ./scripts/appveyor_1.msys build"
 
