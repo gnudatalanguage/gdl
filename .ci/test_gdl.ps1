d5ee82fad895980911af7976051dc19782f968a6
U-DESKTOP-HSNAGG8\pjb7687 <pjb7687@gmail.com>
2021-01-06 23:35:06 +0100

    ps to cmd

--- .ci/test_gdl.ps1
+++
@@ -1,5 +0,0 @@
-# powershell script called from appveyor.yml in the build_script: section
-# This is needed because any write to stderr, which is used by gcc and cmake,
-# will invoke a powershell error.
-
-C:\msys64\usr\bin\bash.exe -lc "cd /c/projects/gdl &&. bash ./.ci/build_gdl_windows.msys test"
