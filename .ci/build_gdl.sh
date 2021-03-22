13eeacb8e84494aef7a3050ab26983efd9f5d2d6
Jeongbin Park <pjb7687@gmail.com>
2021-03-22 01:56:46 +0100

    add latest apt packages

--- .ci/build_gdl.sh
+++ .ci/build_gdl.sh
@@ -36,10 +36,10 @@ elif [ ${BUILD_OS} == "Linux" ]; then
     # JP: Note the seperator ',' between the package name candidates below. The leftmost one has the highest priority.
     # Debian, Ubuntu, Linux Mint, Elementary OS, etc.
     APT_PACKAGES=(
-        libncurses-dev libreadline-dev,libreadline-gplv2-dev zlib1g-dev libpng-dev libgsl-dev,libgsl0-dev libwxgtk3.0-dev,libwxgtk2.8-dev
-        libplplot-dev libgraphicsmagick++1-dev,libgraphicsmagick++-dev libtiff-dev libgeotiff-dev libnetcdf-dev libhdf4-alt-dev
-        libhdf5-dev libfftw3-dev libproj-dev libopenmpi-dev libpython-dev python-numpy libudunits2-dev libeigen3-dev
-        libeccodes-dev,libgrib-api-dev libglpk-dev shapelib libexpat1-dev
+        libncurses-dev libreadline-dev,libreadline-gplv2-dev zlib1g-dev libpng-dev libgsl-dev,libgsl0-dev
+        libwxgtk3.0-gtk3-dev,libwxgtk3.0-dev,libwxgtk2.8-dev libplplot-dev libgraphicsmagick++1-dev,libgraphicsmagick++-dev libtiff-dev
+        libgeotiff-dev libnetcdf-dev libhdf4-alt-dev libhdf5-dev libfftw3-dev libproj-dev libopenmpi-dev libpython3-dev,libpython-dev
+        python3-numpy,python-numpy libudunits2-dev libeigen3-dev libeccodes-dev,libgrib-api-dev libglpk-dev shapelib libexpat1-dev
     )
     # Redhat, Fedora, Scientific Linux, CentOS, openSuSE, SLES, etc.
     RPM_PACKAGES=(
