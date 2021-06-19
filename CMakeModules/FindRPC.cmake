ebf7bb8d4818029d9305c7e394f7eecf54ef3303
GillesDuvert <gilles.duvert@free.fr>
2021-06-19 23:53:12 +0200

    FindRPC was not finding libtirpc if present in some cases, better version.

--- CMakeModules/FindRPC.cmake
+++ CMakeModules/FindRPC.cmake
@@ -1,8 +1,10 @@
 
 include(FindPackageHandleStandardArgs)
 find_path(RPC_INCLUDE_DIR NAMES "rpc/rpc.h" PATH_SUFFIXES "tirpc")
-find_library(RPC_LIBRARY NAMES tirpc)
-if (NOT RPC_LIBRARY-NOTFOUND)
+#is rpc from libtirpc?
+check_library_exists(tirpc xdr_uint64_t "" HAS_LIBTIRPC)
+if(HAS_LIBTIRPC)
+	find_library(RPC_LIBRARY NAMES tirpc)
 	set(RPC_LIBRARIES ${RPC_LIBRARY})
 	find_package_handle_standard_args(RPC DEFAULT_MSG RPC_INCLUDE_DIR RPC_LIBRARY RPC_LIBRARIES)
 else()
