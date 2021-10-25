4bcf4ea02a8396bcb2626a37af7c18dabf42aa72
Oliver Gressel <gressel@nbi.ku.dk>
2021-10-25 16:06:31 +0200

    in routines 'H5[AD]_read_fun()', create the 'elem_dtype_guard' *outside* the conditional block for correct scope (this caused an invalid HDF5 handle for 'elem_dtype', leading to all datasets being created as 'GDL_DOUBLE').

--- src/hdf5_fun.cpp
+++ src/hdf5_fun.cpp
@@ -587,19 +587,15 @@ hid_t
 
        elem_dtype = H5Tget_super(datatype);
        if (elem_dtype < 0) { string msg; e->Throw(hdf5_error_message(msg)); }
-       hdf5_type_guard elem_dtype_guard = hdf5_type_guard(elem_dtype);
-
-    } else if (H5Tget_class(datatype)==H5T_STRING ) {
-
-       elem_dtype = datatype;
 
     } else {
 
        elem_dtype = H5Tcopy(datatype);
        if (elem_dtype < 0) { string msg; e->Throw(hdf5_error_message(msg)); }
-       hdf5_type_guard elem_dtype_guard = hdf5_type_guard(elem_dtype);
 
     }
+
+    hdf5_type_guard elem_dtype_guard = hdf5_type_guard(elem_dtype);
 
     // determine the rank and dimension of the dataset
     int rank = H5Sget_simple_extent_ndims(h5s_id);
@@ -770,19 +766,15 @@ hid_t
 
        elem_dtype = H5Tget_super(datatype);
        if (elem_dtype < 0) { string msg; e->Throw(hdf5_error_message(msg)); }
-       hdf5_type_guard elem_dtype_guard = hdf5_type_guard(elem_dtype);
-
-    } else if (H5Tget_class(datatype)==H5T_STRING ) {
-
-       elem_dtype = datatype;
 
     } else {
 
        elem_dtype = H5Tcopy(datatype);
        if (elem_dtype < 0) { string msg; e->Throw(hdf5_error_message(msg)); }
-       hdf5_type_guard elem_dtype_guard = hdf5_type_guard(elem_dtype);
 
     }
+
+    hdf5_type_guard elem_dtype_guard = hdf5_type_guard(elem_dtype);
 
     // determine the rank and dimension of the dataset
     int rank = H5Sget_simple_extent_ndims(h5s_id);
