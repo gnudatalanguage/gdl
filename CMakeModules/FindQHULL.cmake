bb58565d9c4fbcda20165fe224e61475bdec91ea
GillesDuvert <gilles.duvert@free.fr>
2022-01-10 11:50:27 +0100

    enable finding qhull on my machine, which is the bare minimum!

--- CMakeModules/FindQHULL.cmake
+++ CMakeModules/FindQHULL.cmake
@@ -10,7 +10,8 @@ find_path(QHULL_INCLUDE_DIR NAMES libqhullcpp/Qhull.h
         )
 
 if(QHULL_INCLUDE_DIR)
-    find_library(QHULL_R_LIBRARY NAMES qhullstatic_r HINTS ${CMAKE_PREFIX_PATH})
+    find_library(QHULL_R_STATIC_LIBRARY NAMES qhullstatic_r HINTS ${CMAKE_PREFIX_PATH})
+    find_library(QHULL_R_DYNAMIC_LIBRARY NAMES qhull_r HINTS ${CMAKE_PREFIX_PATH})
     find_library(QHULL_CPP_LIBRARY NAMES qhullcpp HINTS ${CMAKE_PREFIX_PATH})
 
     # include(FindPackageHandleStandardArgs)
@@ -18,6 +19,13 @@ if(QHULL_INCLUDE_DIR)
     #     QHULL
     #     REQUIRED_VARS QHULL_R_LIBRARY QHULL_CPP_LIBRARY QHULL_INCLUDE_DIR
     #     )
+    if (QHULL_R_STATIC_LIBRARY)
+      SET(QHULL_R_LIBRARY ${QHULL_R_STATIC_LIBRARY})
+    endif (QHULL_R_STATIC_LIBRARY)
+      
+    if (QHULL_R_DYNAMIC_LIBRARY)
+      SET(QHULL_R_LIBRARY ${QHULL_R_DYNAMIC_LIBRARY})
+    endif (QHULL_R_DYNAMIC_LIBRARY)
 
     if(QHULL_R_LIBRARY AND QHULL_CPP_LIBRARY)
         SET(QHULL_FOUND TRUE)
@@ -26,7 +34,7 @@ if(QHULL_INCLUDE_DIR)
     elseif(NOT QHULL_CPP_LIBRARY)
         message(STATUS "FindQhull: Could not find QHULL C++ library libqhullcpp")
     else()
-        message(STATUS "FindQhull: Could not find QHULL library libqhullstatic_r")
+        message(STATUS "FindQhull: Could not find QHULL Reentrant library libqhull[static]_r")
     endif()
 else(QHULL_INCLUDE_DIR)
     message(STATUS "FindQhull: Could not find QHULL include directories.")
