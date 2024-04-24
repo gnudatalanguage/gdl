dfc61d5ce6396b6baa74dcbaa95e79791a635db2
GillesDuvert <gilles.duvert@free.fr>
2024-04-24 20:03:16 +0200

    removed (test) MPI for OSX (started to bug  2 weeks ago)

--- scripts/build_gdl.sh
+++ scripts/build_gdl.sh
@@ -441,12 +441,12 @@ function configure_gdl {
     if [[ ${BUILD_OS} == "macOS" ]]; then
         if [[ ${Platform} == "arm64" ]]; then
             export LIBRARY_PATH=$LIBRARY_PATH:/opt/homebrew/opt/llvm/lib
-            CMAKE_ADDITIONAL_ARGS=( "-DREADLINEDIR=/opt/homebrew/opt/readline"
+            CMAKE_ADDITIONAL_ARGS=( "DMPI=OFF -DREADLINEDIR=/opt/homebrew/opt/readline"
                                     "-DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++"
                                     "-DCMAKE_C_COMPILER=/opt/homebrew/opt/llvm/bin/clang" ) 
         else
             export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/llvm/lib
-            CMAKE_ADDITIONAL_ARGS=( "-DREADLINEDIR=/usr/local/opt/readline"
+            CMAKE_ADDITIONAL_ARGS=( "DMPI=OFF -DREADLINEDIR=/usr/local/opt/readline"
                                     "-DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++"
                                     "-DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang" )
         fi
