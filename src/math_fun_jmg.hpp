4d8ba086cf60fbe0ff5c4f8591e747385abf6cc8
GillesDuvert <gilles.duvert@free.fr>
2024-11-26 14:32:50 +0100

    rewrite poly_2d for 1) better treatment of 'borders' 2) SOLVE #1909 (Use Floats for coordinates) 3) much improved speed ! rewrite congrid to use poly_2d similarly to IDL. Removed all tentatives to 'do better' (with dimensions). separate poly_2d from math_fun_jmg add poly_2d basic test

--- src/math_fun_jmg.hpp
+++ src/math_fun_jmg.hpp
@@ -20,48 +20,13 @@
 
 #include "envt.hpp"
 
-typedef struct _2D_POLY_ {
-        DLong  nc;            /* number of coefficients in px, py, c */
-        DLong* px;            /* powers of x                         */
-        DLong* py;            /* powers of y                         */
-        DDouble* c;           /* polynomial coefficients             */
-} poly2d ;
-
-typedef double	pixelvalue ;
-
-typedef struct _image_
-{
-	/* Size of the image in x and y */
-    int             	lx, ly ;
-	/* Pointer to pixel buffer as a 1d buffer */
-    pixelvalue      *	data ;
-} image_t ;
-
-#define TABSPERPIX      (1000)
-#define KERNEL_WIDTH    (2.0)
-#define KERNEL_SAMPLES  (1+(int)(TABSPERPIX * KERNEL_WIDTH))
-#define PI_NUMB     (3.1415926535897932384626433832795)
-#define MAX_COLUMN_NUMBER               (40000)
-#define MAX_LINE_NUMBER                 (40000)
-
-#define LINEAR  1
-#define GENERIC 2
- 
 namespace lib {
 
-//   BaseGDL* transpose_fun( EnvT* e);
   BaseGDL* machar_fun( EnvT* e);
   BaseGDL* finite_fun( EnvT* e);
   BaseGDL* check_math_fun( EnvT* e);
   BaseGDL* radon_fun( EnvT* e);
-  BaseGDL* poly_2d_fun( EnvT* e);
-  BaseGDL* poly_2d_funnew( EnvT* e);
   BaseGDL* rk4jmg_fun( EnvT* e);
-
-  double ipow(double x, int p);
-//  double sinc(double x);
-  double poly2d_compute(poly2d *p, double x, double y);
-  DDouble * generate_interpolation_kernel(int kernel_type, DDouble cubic);
 
 } // namespace
 
