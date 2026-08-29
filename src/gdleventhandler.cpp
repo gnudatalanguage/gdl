b5b1dec5895c54a776a5fbd9028ec2305715c2a7
Giloo <gildas@localhost>
2026-08-29 12:40:43 +0200

    make duration static

--- src/gdleventhandler.cpp
+++ src/gdleventhandler.cpp
@@ -33,6 +33,8 @@
 #endif
 
 using namespace std;
+const long SLEEP = 10000000; // 10ms
+static struct timespec delay={0,SLEEP};
 
 int GDLEventHandler()
 {
@@ -48,10 +50,6 @@ int GDLEventHandler()
 #ifdef _WIN32 
     Sleep(10); // this just to quiet down the character input from readline. 2 was not enough. 20 was ok.
 #else
-    const long SLEEP = 10000000; // 10ms
-    struct timespec delay;
-    delay.tv_sec = 0;
-    delay.tv_nsec = SLEEP;
     nanosleep(&delay, NULL);
 #endif  
   return 0;
